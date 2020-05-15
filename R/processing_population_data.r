657d8e3257058f916f8f4cffadb0984f77542c42
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-05-15 14:02:58 +0100

    Revise hdf5 files structure

--- R/processing_population_data.r
+++ R/processing_population_data.r
@@ -3,26 +3,15 @@
 h5filename <- "scrc_demographics.h5"
 
 
-# Create file
+# Create hdf5 file
 rhdf5::h5createFile(h5filename)
 
-# Create group for raw data
-rhdf5::h5createGroup(h5filename, "rawdata")
-# Create subgroup for raw population data
-rhdf5::h5createGroup(h5filename, "rawdata/scotlandpopulations")
-# Create subgroup for SIMD deprivation data
-rhdf5::h5createGroup(h5filename, "rawdata/SIMD")
-
-# Create group for processed data
-rhdf5::h5createGroup(h5filename, "processeddata")
-# Create subgroup for processed population data
-rhdf5::h5createGroup(h5filename, "processeddata/scotlandpopulations_single_year")
+# Create group for datazones and subgroup for scotland2018
+rhdf5::h5createGroup(h5filename, "datazone")
+rhdf5::h5createGroup(h5filename, "datazone/scotland2018")
 
 # Create group for grid data
 rhdf5::h5createGroup(h5filename, "griddata")
-
-# h5ls(h5filename) %>% View()
-
 
 
 
@@ -32,9 +21,6 @@ filename <- "data-raw/population_datazone/sape-2018-persons.xlsx"
 
 # Read raw data and export to h5 file
 sape_persons <- readxl::read_excel(filename, col_names = FALSE)
-
-rhdf5::h5write(sape_persons, file = h5filename,
-        name = file.path("rawdata/scotlandpopulations", "datazone_population_persons_singleyear_2018"))
 
 # Extract header
 header <- readxl::read_excel(filename, skip = 3, n_max = 2) 
@@ -61,17 +47,16 @@ sape_persons %<>%
         dplyr::select_at(vars(-dplyr::ends_with("Name"),
                        -AllAges)) %>% 
         dplyr::mutate_at(vars(dplyr::starts_with("AGE")), as.numeric)
+
 h5write(sape_persons, file = h5filename, 
-        name = file.path("processeddata/scotlandpopulations_single_year",
-                         "datazone_population_persons_singleyear_2018"))
+        name = file.path("datazone/scotland2018"))
 
 
 # Metadata/attributes -----------------------------------------------------
 
 fid <- H5Fopen(h5filename)
 
-H5D_name <- file.path("/processeddata/scotlandpopulations_single_year",
-                      "datazone_population_persons_singleyear_2018")
+H5D_name <- file.path("/datazone/scotland2018")
 did <- H5Dopen(fid, name = H5D_name) 
 H5D_attr <- paste("Population of datazones in Scotland in 2018 contains data for single year of age from age 0 to 89 and a 90+ age class and all ages, data from National Records Scotland, available at https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/population/population-estimates/2011-based-special-area-population-estimates/small-area-population-estimates/time-series. Downloaded 24/4/20. Processed to remove unnecessary identifier columns/rows. Raw data available: /rawdata/scotlandpopulations/datazone_population_persons_singleyear_2018")
 h5writeAttribute(did, name = "Description", attr = H5D_attr)
