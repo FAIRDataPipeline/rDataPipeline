3fb069fa35e91bd8ec07d7ef2704c4284afb074b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-05-29 14:32:06 +0100

    edits to documentation

--- R/demographicData-package.R
+++ R/demographicData-package.R
@@ -1,15 +1,14 @@
 #' demographicData
 #'
 #' scrc demographicData package
-#' 
+#'
 #' @name demographicData-package
 #' @aliases demographicData
 #' @docType package
-#' 
+#'
 #' @importFrom assertthat assert_that
 #' @import dplyr
 #' @import readxl
-#' @import rhdf5
 #' @import sf
 #' @import tibble
 #' @import tidyr
