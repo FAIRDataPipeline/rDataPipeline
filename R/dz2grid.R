25a7292849ed438e00b095f9229cedd98c6ea9eb
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-08 10:07:51 +0100

    update documentation

--- R/dz2grid.R
+++ R/dz2grid.R
@@ -2,31 +2,6 @@
 #'
 #' This script transforms age class population data from census geographies to
 #' a grid based system.
-#' If the census geography is split across two grid cells this script splits
-#' the population between these cells according to the proportion of the
-#' postcodes in the geography which are present in each cell.
-#' i.e. if a datazone containing 5 postcodes is split between 2 grid cells
-#' with 3 postcodes in cell A and 2 in cell B, 60% of the population goes to cell A.
-#' It takes:
-#' - shapefile of the census geographies from which the populations should
-#' be drawn
-#' - a population dataset containing single-year age classes for each
-#' census geography (alternatively a population dataset with preset age
-#' classes can be used) in the structure: 1st column - Geographical identifier,
-#' columns 2:92 single year population totals for age 0 to 89 and 90+ ageclass
-#' - A grid shapefile which overlays the census geographies. (IF this is not
-#' available this script automatically makes a grid, the dimensions of which
-#' can be chosen (in m))
-#' - A shapefile containing the boundaries of postcodes contained in the
-#' census geographies
-#'
-#' If a single-year age class dataset is supplied and a different structure is
-#' desired (i.e. 5-year age classes) the structure of this new age class must
-#' be set as a vector called: age_class_structure
-#' This vector should contain the lower age bound of each age class
-#'
-#' There is an issue of rounding when the population are being divided between
-#' cells, I have a work-around which is explained in the code.
 #'
 #' @param dat datazone population data
 #' @param datazones datazones
