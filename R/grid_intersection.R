75159024829df574b13ec63ad994f19a82573c00
Nardus Mollentze <nardus.mollentze@glasgow.ac.uk>
2020-06-09 19:07:17 +0100

    Fix stringsAsFactors bug
    Grid labels returned by grid_intersection were factors in R versions which have stringsAsFactors = TRUE as the default. This might cause issues when converting to numeric.

--- R/grid_intersection.R
+++ R/grid_intersection.R
@@ -11,15 +11,13 @@ grid_intersection <- function(datazones, gridsize) {
   num_columns <- ceiling(width / n)
   num_rows <- ceiling(height / n)
   grid_labels <- paste0(1:num_columns, "-", rep(1:num_rows, each = num_columns))
-  grid_matrix <- strsplit(grid_labels, "-") %>% do.call(rbind.data.frame, .)
+  grid_matrix <- strsplit(grid_labels, "-") %>% lapply(as.numeric) %>% do.call(rbind.data.frame, .)
   colnames(grid_matrix) <- c("x", "y")
-  grid_matrix <- grid_matrix %>%
-    dplyr::mutate_if(is.character, as.numeric)
 
   grids <- sf::st_sf(grids, grid_id = grid_labels)
 
-  assertthat::assert_that(max(as.numeric(grid_matrix$x)) == num_columns)
-  assertthat::assert_that(max(as.numeric(grid_matrix$y)) == num_rows)
+  assertthat::assert_that(max(grid_matrix$x) == num_columns)
+  assertthat::assert_that(max(grid_matrix$y) == num_rows)
 
   dz_subdivisions <- sf::st_intersection(grids, datazones)
 
