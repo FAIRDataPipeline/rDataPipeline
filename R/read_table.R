94865add757b63ef06ef5c8011e2088355541908
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-12 16:50:21 +0100

    read_table() initial commit

---
+++ R/read_table.R
@@ -0,0 +1,13 @@
+#' read_table
+#'
+#' @export
+#'
+read_table <- function(h5filename, path) {
+  file.h5 <- H5File$new(h5filename, mode = "r+")
+
+  object <- file.h5[[paste0(path, "/table")]][]
+  rownames(object) <- file.h5[[paste0(path, "/row_names")]][]
+
+  file.h5$close_all()
+  object
+}
