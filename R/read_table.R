2b0eea55b6ff773d9ddd18d1ba6c15525781eaac
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-18 16:17:46 +0100

    row_names are optional

--- R/read_table.R
+++ R/read_table.R
@@ -6,7 +6,8 @@ read_table <- function(h5filename, path) {
   file.h5 <- H5File$new(h5filename, mode = "r")
 
   object <- file.h5[[paste0(path, "/table")]][]
-  rownames(object) <- file.h5[[paste0(path, "/row_names")]][]
+  if(any("row_names" %in% names(file.h5[[path]])))
+    rownames(object) <- file.h5[[paste0(path, "/row_names")]][]
 
   file.h5$close_all()
   object
