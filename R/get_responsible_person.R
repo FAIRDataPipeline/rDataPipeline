c63cf5e757557e5f1b984704a98cb6c4a8f3aa03
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-22 11:47:32 +0100

    get responsible_person url from db

---
+++ R/get_responsible_person.R
@@ -0,0 +1,19 @@
+#' get_responsible_person
+#'
+#' @param full_name
+#'
+#' @export
+#'
+get_responsible_person <- function(full_name) {
+
+  tmp <- httr::GET(file.path(endpoint, "users", ""),
+                   httr::add_headers(.headers = headers)) %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON(simplifyVector = FALSE)
+
+  ind <- lapply(tmp, function(x) x$full_name == full_name) %>%
+    unlist() %>%
+    which()
+
+  tmp[[ind]]$url
+}
