55e04aba4e445a47eef6bd499252c27f078382ac
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-22 11:47:21 +0100

    add new storage root to db

---
+++ R/new_storage_root.R
@@ -0,0 +1,17 @@
+#' new_storage_root
+#'
+#' @param name name of the storage root, e.g. "Boydorr"
+#' @param description free text description of the storage root, e.g. "Boydorr
+#' FTP server"
+#' @param uri link to the root of the storage, e.g. "ftp://srv/ftp/scrc"
+#' @param type reference to the storage type used
+#'
+#' @export
+#'
+new_storage_root <- function(name, description, uri, type) {
+
+  list(name = name,
+       description = description,
+       uri = uri,
+       type = type)
+}
