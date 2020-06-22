4ac1b3bb9ae986beb8f3c4265a25b0c560fa4975
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-22 10:52:26 +0100

    Function to generate storage_type meta data

---
+++ R/new_storage_type.R
@@ -0,0 +1,12 @@
+#' new_storage_type
+#'
+#' @param name
+#' @param description
+#'
+#' @export
+#'
+new_storage_type <- function(name,
+                             description) {
+  list(name = name,
+       description = description)
+}
