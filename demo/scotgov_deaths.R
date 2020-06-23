18a6b163a114330d7756dad4d2651cb8db402659
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-06-23 23:51:58 +0100

    upload source data and processing script metadata to database

--- demo/scotgov_deaths.R
+++ demo/scotgov_deaths.R
@@ -7,12 +7,38 @@ library(SCRCdataAPI)
 library(SPARQL)
 
 # Download source data
-download_source_version(dataset = "scotgov_deaths")
+source_path <- file.path("human", "infection", "SARS-CoV-2", "scotland", "mortality")
+
+post_source_data(
+  storage_type = "ftp",
+  storage_root = "Boydorr",
+  storage_location = source_path,
+  accessibility = "public",
+  source_type = "database",
+  source = "statistics.gov.scot",
+  source_version_identifier = 1,
+  source_version_description = "2020-06-23",
+  responsible_person = "Sonia Mitchell",
+  source_version_supercedes = "",
+  key = read.table("token.txt"))
 
 # Process data and generate hdf5 file
-sourcefile <- "data-raw/deaths-involving-coronavirus-covid-19.csv"
-h5filename <- "deaths-involving-coronavirus-covid-19.h5"
-process_scot_gov_deaths(sourcefile = sourcefile,
-                        h5filename = h5filename)
+process_scot_gov_deaths(
+  sourcefile = "data-raw/deaths-involving-coronavirus-covid-19.csv",
+  h5filename = "deaths-involving-coronavirus-covid-19.h5")
 
-openssl::sha256(file(h5filename))
+
+script_path <- file.path("R", "process_scotgov_deaths.R")s
+post_processing_script(storage_type = "GitHub",
+                       storage_root = "SCRCdataAPI",
+                       storage_location = script_path,
+                       accessibility = "public",
+                       processing_script = "process_scotgov_deaths.R",
+                       script_version_identifier = 1,
+                       responsible_person = "Sonia Mitchell",
+                       script_version_supercedes = "",
+                       key = read.table("token.txt"))
+
+post_data_product()
+
+
