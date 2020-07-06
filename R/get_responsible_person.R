5953ea1778de9b7174298a2e252cb0ec10235857
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-06 22:56:10 +0100

    Get responsible person from github config

--- R/get_responsible_person.R
+++ R/get_responsible_person.R
@@ -1,20 +1,20 @@
 #' get_responsible_person
 #'
-#' @param full_name
 #' @param key
 #'
 #' @export
 #'
-get_responsible_person <- function(full_name, key) {
+get_responsible_person <- function(key) {
 
   h <- c(Authorization = paste("token", key))
+  username <- system2("git", "config user.name", stdout = TRUE)
 
   tmp <- httr::GET(file.path("https://data.scrc.uk/api", "users", ""),
                    httr::add_headers(.headers = h)) %>%
     httr::content(as = "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON(simplifyVector = FALSE)
 
-  ind <- lapply(tmp, function(x) x$full_name == full_name) %>%
+  ind <- lapply(tmp, function(x) x$username == username) %>%
     unlist() %>%
     which()
 
