d39265e5407d8ac02d914571eeef991724c9e0b3
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-06 16:06:15 +0100

    Give number a name

--- R/read_number.R
+++ R/read_number.R
@@ -3,5 +3,6 @@
 #' @export
 #'
 read_number <- function(toml_filename) {
-  configr::read.config(file = toml_filename)
+  tmp <- configr::read.config(file = toml_filename)$`point-estimate`
+  setNames(as.numeric(tmp$value), tmp$name)
 }
