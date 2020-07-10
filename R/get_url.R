c406809e9ad484a79e647b66b405e08f08cc149a
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-10 11:44:51 +0100

    filtering works again. query database rather than filter in R.

--- R/get_url.R
+++ R/get_url.R
@@ -7,18 +7,17 @@
 #'
 get_url <- function(table, query = list()) {
 
-  # tmp <- httr::GET(file.path("https://data.scrc.uk/api", table, ""),
-  #                  query = query) %>%
-  #   httr::content(as = "text", encoding = "UTF-8") %>%
-  #   jsonlite::fromJSON(simplifyVector = FALSE)
-  #
-  # if(length(tmp) > 1) {
-  #   stop("More than 1 object was returned")
-  # } else if(length(tmp) == 0) {
-  #   stop("No objects were returned")
-  # } else {
-  #   return(tmp[[1]]$url)
-  # }
-  tmp <- get_existing(table)
-  tmp$url[which(tmp[,names(query)] == unname(query))]
+  tmp <- httr::GET(file.path("https://data.scrc.uk/api", table, ""),
+                   query = query) %>%
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(simplifyVector = FALSE)
+
+  if(length(tmp) > 1) {
+    stop("More than 1 object was returned")
+  } else if(length(tmp) == 0) {
+    stop("No objects were returned")
+  } else {
+    return(tmp[[1]]$url)
+  }
+
 }
