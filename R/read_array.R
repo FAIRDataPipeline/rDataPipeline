86122041d068f487d6ab3db60dd21c6eb5d0d581
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-11 13:02:16 +0100

    fix problem reading 1 dimensional arrays.

--- R/read_array.R
+++ R/read_array.R
@@ -6,7 +6,7 @@ read_array <- function(h5filename, path) {
   file.h5 <- H5File$new(h5filename, mode = "r")
 
   object <- file.h5[[paste0(path, "/array")]][,]
-  if(is.vector(object)) object <- matrix(object)
+  if(is.vector(object)) object <- t(matrix(object))
   object <- as.data.frame(object)
   colnames(object) <- file.h5[[paste0(path, "/Dimension_2_names")]][]
   rownames(object) <- file.h5[[paste0(path, "/Dimension_1_names")]][]
