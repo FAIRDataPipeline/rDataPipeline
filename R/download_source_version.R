fbd3d1911a1c4c4afe96290885a8623c8b57729c
ciaranmcmonagle <c.mcmonagle.1@research.gla.ac.uk>
2020-07-12 23:25:03 +0100

    Remove nomisr

--- R/download_source_version.R
+++ R/download_source_version.R
@@ -34,33 +34,36 @@ download_source_version <- function(dataset) {
   } else if(dataset == "ons_eng_wales_population") {
 
     # ons_eng_wales_population ----------------------------------------------
-
-    genders <- c("Persons", "Males", "Females")
-
-    for(sex in seq_along(genders)) {
-      for(age in 101:191) {
-        temp_pop_table <- nomisr::nomis_get_data(id = "NM_2010_1",
-                                               time = "latest",
-                                               geography = c("TYPE299"),
-                                               gender = c(sex-1),
-                                               c_age = age,
-                                               measures = 20100) %>%
-          dplyr::select(DATE, GEOGRAPHY_NAME, GEOGRAPHY_CODE, GEOGRAPHY_TYPE,
-                        GENDER_NAME, C_AGE_NAME, MEASURES_NAME, OBS_VALUE)
-        names(temp_pop_table)[8] <- unique(temp_pop_table$C_AGE_NAME)
-        geography_value <- temp_pop_table[,c(2,8)]
-
-        if(age == 101) {
-          population_table = geography_value
-
-        } else {
-          population_table = left_join(population_table,
-                                       geography_value,
-                                       by = "GEOGRAPHY_NAME")
+    for (sex in seq_along(genders)) {
+      for (age in 101:191) {
+        for (step in c(0,((c(24000)*c(1:7))))){
+          download_from_url(url="https://www.nomisweb.co.uk"
+                            , path=sprintf("api/v01/dataset/NM_2010_1.data.csv?measures=20100&time=latest&geography=TYPE299&gender=%d&c_age=%d&RecordLimit=24000&RecordOffset=%d", sex-1,age, step ), 
+                            local="data-raw", filename=sprintf("populationstore_g%d_a%d_s%d.csv", sex-1,age, step ))
+          temp_pop_table <-read.csv(sprintf("data-raw/populationstore_g%d_a%d_s%d.csv", sex-1,age, step )) %>%
+            dplyr::select(DATE, GEOGRAPHY_NAME, GEOGRAPHY_CODE, 
+                          GEOGRAPHY_TYPE, GENDER_NAME, C_AGE_NAME, 
+                          MEASURES_NAME, OBS_VALUE)
+          file.remove(sprintf("data-raw/populationstore_g%d_a%d_s%d.csv", sex-1,age, step ))
+          names(temp_pop_table)[8] <- unique(temp_pop_table$C_AGE_NAME)
+          geography_value <- temp_pop_table[, c(2, 8)]
+          
+          if(step==0){
+            temp_population_table=geography_value
+          }else{
+            temp_population_table=rbind(temp_population_table,geography_value)
+          }
+        }
+        if (age == 101) {
+          population_table = temp_population_table
+        }
+        else {
+          population_table = left_join(population_table, 
+                                       geography_value, by = "GEOGRAPHY_NAME")
         }
       }
-
-      write.csv(population_table, paste0("england_", genders[sex], ".csv"))
+      write.csv(population_table, paste0("data-raw/england_", genders[sex], 
+                                         ".csv"))
     }
 
   } else if(dataset == "nrs_demographics") {
