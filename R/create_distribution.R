368341e86a307ce28f7915f67d06c72091eb0d6c
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-14 13:13:50 +0100

    change argument name

--- R/create_distribution.R
+++ R/create_distribution.R
@@ -5,7 +5,7 @@
 #' @param filename a \code{string} specifying the name of the toml file, e.g.
 #' "0.1.0.toml"
 #' @param path a \code{string} specifying the name of the toml file (and path)
-#' @param descriptor a \code{string} specifying an associated descriptor
+#' @param name a \code{string} specifying the name of the parameter
 #' @param distribution a \code{string} specifying the name of the distribution
 #' @param parameters a named \code{list} specifying parameter values and
 #' associated names (see example)
@@ -14,15 +14,15 @@
 #'
 #' @examples
 #' filename <- "test.toml"
-#' path <- "data-raw/latency"
-#' descriptor <- "latency"
-#' distribution <- "gamma"
+#' path <- "data-raw/example"
+#' name <- "latency"
+#' distribution = "gamma"
 #' parameters <- list(shape = 2.0, scale = 3.0)
-#' create_distribution(filename, path, descriptor, distribution, parameters)
+#' create_distribution(filename, path, name, distribution, parameters)
 #'
 create_distribution <- function(filename,
                                 path,
-                                descriptor,
+                                name,
                                 distribution,
                                 parameters) {
   # Check file name
@@ -37,6 +37,6 @@ create_distribution <- function(filename,
     paste0(names(parameters[x]), " = ", parameters[x], "\n")) %>%
     paste0(collapse = "")
 
-  cat(paste0("[", descriptor, "]\ntype = \"distribution\"\ndistribution = \"",
+  cat(paste0("[", name, "]\ntype = \"distribution\"\ndistribution = \"",
              distribution, "\"\n", tmp, "\n"), file = file.path(path, filename))
 }
