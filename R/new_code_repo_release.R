6d423abe9af501d718df16891f4b18e27c98256b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-15 23:44:18 +0100

    rename file

--- R/new_code_repo_release.R
+++ R/new_code_repo_release.R
@@ -3,7 +3,7 @@
 #' @param name e.g.
 #' @param version e.g.
 #' @param website e.g.
-#' @param object e.g.
+#' @param object_id e.g.
 #' @param key key
 #'
 #' @export
@@ -11,15 +11,13 @@
 new_code_repo_release <- function(name,
                                   version,
                                   website,
-                                  object,
+                                  object_id,
                                   key) {
-
-  object_url <- get_url("object", list(id = object))
 
   post_data(table = "code_repo_release",
             data = list(name = name,
                         version = version,
                         website = website,
-                        object = object_url),
+                        object = object_id),
             key)
 }
