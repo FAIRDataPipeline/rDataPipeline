235a75ca27749abc208218ebca1c8aec05499179
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-28 10:52:51 +0100

    change "" to NULL for query

--- R/clean_query.R
+++ R/clean_query.R
@@ -8,5 +8,9 @@ clean_query <- function(data) {
   data_tmp <- lapply(data, function(x) {
     ifelse(grepl("https://data.scrc.uk/api", x), basename(x), x)
   })
+  if(any(data_tmp == "")) {
+    data_tmp[which(data_tmp == "")] <- list(NULL)
+  }
+
   return(data_tmp)
 }
