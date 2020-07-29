b34e78d1ba45bf0a50f5e5dc56374678af91385d
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-29 16:12:14 +0100

    remove function

--- R/read_distribution.R
+++ R/read_distribution.R
@@ -3,8 +3,6 @@
 #' @param filename a \code{string} specifying the name of the toml file, e.g.
 #' "0.1.0.toml"
 #'
-#' @export
-#'
 read_distribution <- function(filename) {
   tmp <- configr::read.config(file = filename)
   what <- tmp[[1]]$type
