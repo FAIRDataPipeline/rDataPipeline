fa44c6209bf40925f4033cccdba68edb9b4dddd8
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-30 10:11:24 +0100

    edit tests

--- tests/testthat/test_create_table.R
+++ tests/testthat/test_create_table.R
@@ -1,30 +1,33 @@
 library(hdf5r)
+library(SCRCdataAPI)
 
 context("Testing create_table()")
 
 filename <- "test_table.h5"
 component <- "level"
 
-test_that("function behaves as it should", {
-  # Incorrect file name should throw error
+test_that("incorrect file name throws an error", {
   testthat::expect_error(create_table(filename = "test_table",
                                       component = component,
                                       df = data.frame(a = 1:2, b = 3:4)))
+})
 
-  # Incorrect df format should throw error
+test_that("incorrect df format throws an error", {
   testthat::expect_error(
     create_table(filename = filename,
                  component = component,
                  df = as.matrix(data.frame(a = 1:2, b = 3:4)))
   )
+})
 
-  # File should be h5 format
+test_that(".h5 file is generated", {
   create_table(filename = filename,
                component = component,
                df = data.frame(a = 1:2, b = 3:4))
   testthat::expect_true(hdf5r::is.h5file(filename))
+})
 
-  # Component name should be "level"
+test_that("component name is level", {
   file.h5 <- H5File$new(filename, mode = "r")
   testthat::expect_equal(names(file.h5), component)
   file.h5$close_all()
@@ -33,3 +36,25 @@ test_that("function behaves as it should", {
 
 # Remove test file
 file.remove(filename)
+
+
+test_that("function behaves as it should", {
+  create_table(filename = filename,
+               component = component,
+               df = data.frame(a = letters[1:2], b = 3:4))
+
+  read_table(filename = filename,
+             path = ".",
+             component = component)
+})
+
+
+
+
+
+
+
+
+
+
+
