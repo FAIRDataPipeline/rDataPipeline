d3914730efd0c08b5280dc92538c4ae3e3e29ac9
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-07-31 16:19:15 +0100

    fix argument filtering and document

--- R/attach_issue.R
+++ R/attach_issue.R
@@ -8,23 +8,23 @@
 #' * a component within a data product (in which case you must specify the
 #' \code{namespace}, \code{data_product}, and \code{component} arguments)
 #'
+#' @param description *e.g.* "Data dump caused a spike on the 15th of June"
+#' @param severity *e.g.* 19
 #' @param external_object *e.g.*
 #' "scottish coronavirus-covid-19-management-information"
 #' @param namespace *e.g.* "SCRC"
 #' @param data_product *e.g.* "records/SARS-CoV-2/scotland/cases_and_management"
 #' @param component *e.g.* "testing_location/date-cumulative"
-#' @param description *e.g.* "Data dump caused a spike on the 15th of June"
-#' @param severity *e.g.* 19
 #' @param key key
 #'
 #' @export
 #'
-attach_issue <- function(external_object,
+attach_issue <- function(description,
+                         severity,
+                         external_object,
                          namespace,
                          data_product,
                          component,
-                         description,
-                         severity,
                          key) {
 
   # Does this issue already exist?
@@ -47,13 +47,9 @@ attach_issue <- function(external_object,
   tmp <- get_entry("issue", list(description = description))
 
 
-  if(!missing(external_object)) {
+  if(missing(namespace) & missing(data_product) & missing(component)) {
 
     # Attach issue to external object -----------------------------------------
-
-    if(!missing(namespace) | !missing(data_product) | !missing(components))
-      stop(paste("Attaching an issue to an external object does not require",
-                 "the namespace, data_product, or component arguments"))
 
     # Which objects are currently associated with the issue?
     current_objects <- tmp$object_issues
@@ -66,6 +62,7 @@ attach_issue <- function(external_object,
     object_issues <- c(current_objects, objectId)
 
     # Attach issue to external object in the data registry
+    message("Attaching issue to external object")
     patch_data(url = issueId,
                data = list(severity = severity,
                            description = description,
@@ -75,15 +72,10 @@ attach_issue <- function(external_object,
 
 
 
-  } else if(!missing(namespace) &
-            !missing(data_product) &
+  } else if(missing(external_object) &
             missing(component)) {
 
     # Attach issue to data product --------------------------------------------
-
-    if(!missing(external_object))
-      stop(paste("Attaching an issue to a data product does not require",
-                 "the component or external_object arguments"))
 
     # Which objects are currently associated with the issue?
     current_objects <- tmp$object_issues
@@ -97,7 +89,8 @@ attach_issue <- function(external_object,
     # Add this to the current list
     object_issues <- c(current_objects, objectId)
 
-    # Attach issue to external object in the data registry
+    # Attach issue to data product in the data registry
+    message("Attaching issue to data product")
     patch_data(url = issueId,
                data = list(severity = severity,
                            description = description,
@@ -107,15 +100,9 @@ attach_issue <- function(external_object,
 
 
 
-  } else if(!missing(namespace) &
-            !missing(data_product) &
-            !missing(component)) {
+  } else if(missing(external_object)) {
 
-    # Attach issue to object components ---------------------------------------
-
-    if(!missing(external_object))
-      stop(paste("Attaching an issue to an object component does not require",
-                 "the external_object argument"))
+    # Attach issue to object component ---------------------------------------
 
     # Which object components are currently associated with the issue?
     current_components <- tmp$component_issues
@@ -126,18 +113,20 @@ attach_issue <- function(external_object,
     objectId <- get_entry("data_product", list(name = data_product,
                                                namespace = namespaceId))$object
     objectId <- clean_query(list(name = objectId))
-    objectComponentId <- get_entry("object_component", list(name = component,
-                                                            object = objectId))$object
+    objectComponentId <- get_url("object_component", list(name = component,
+                                                            object = objectId))
 
     # Add this to the current list
     component_issues <- c(current_components, objectComponentId)
 
-    # Attach issue to external object in the data registry
+    # Attach issue to object component in the data registry
+    message("Attaching issue to object component")
     patch_data(url = issueId,
                data = list(severity = severity,
                            description = description,
                            object_issues = tmp$object_issues,
                            component_issues = component_issues),
                key = key)
-  }
+  } else
+    stop("Please check your arguments have been inputted correctly")
 }
