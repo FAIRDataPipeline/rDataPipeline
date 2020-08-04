5185acf525622ee0bc8b1b82104d5115e4d66ed6
Ryan J Field <ryan@ryanfield.me>
2020-08-04 17:23:45 +0100

    Change message to green if package is up to date

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,15 @@
 .onLoad <- function(...){
   # Check package is up to date
   tryCatch({
-    if(! is_current_version())
-      message("Warning: Your package version is out of date please update")
-    else
-      message("Your package is up to date")
+    if(! is_current_version()){
+      message(paste("Warning: Your package version is out of date please update\n", "Git Version: ", get_remote_package_version(), " Local Version: ", get_loaded_package_version()))}
+    else{
+      if (crayon::has_color())
+        message(crayon::green(paste("Version: ", get_loaded_package_version(), " Your package is up to date")))
+      else
+        message(paste("Version: ", get_loaded_package_version(), " Your package is up to date"))
+    }
+
   }, error = function(e){
     message("Could not check if updates are available, please check manually")
   }, message = function (m){
