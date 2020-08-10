d50c5811ff42a027843b08ddf1dd3653673554be
Ryan J Field <ryan@ryanfield.me>
2020-08-10 18:14:37 +0100

    Fix get_existing() after pagination update add functionality to return all rather than just the 100 api default limit add functionality to only return the URL and id

--- R/get_existing.R
+++ R/get_existing.R
@@ -1,20 +1,62 @@
 #' get_existing
 #'
 #' @param table table
-#'
+#' @param limit_results boolean whether or not to limit
+#' the results default \code{TRUE}
+#' @param detail what level of detail to return
+#' use \code{"all"} for all details or \code{"id"} for just URL and ID's
+#' @return returns a data.frame of entries in table
+#' default is limited to 100 entries
 #' @export
 #'
-get_existing <- function(table) {
+get_existing <- function(table, limit_results = TRUE, detail = "all") {
+
+  if(! check_table_exists(table))
+    stop(paste0("Table: ", table, " does not exist\n", "For available tables use: get_tables()"))
+
 
     output <- httr::GET(file.path("http://data.scrc.uk/api", table, "")) %>%
       httr::content(as = "text", encoding = "UTF-8") %>%
       jsonlite::fromJSON(simplifyVector = FALSE)
-    suppressWarnings(
-    output <- output$results %>%
-      dplyr::bind_rows()
-  )
 
-  if(length(output) == 0)
-    print("Returned no entries") else output
+    results <- output$results
+    # allow function to return more than 100 results
+    if(!limit_results)
+    {
+      # Because pagination is enabled next will only be null if there are no more pages
+      # next is a reserved word so wrap it in ``
+      # while loop to update results from all pages
+      while(!is.null(output$`next`)){
+        tmp_output <- httr::GET(file.path(output$`next`)) %>%
+          httr::content(as = "text", encoding = "UTF-8") %>%
+          jsonlite::fromJSON(simplifyVector = FALSE)
+        results <- c(results, tmp_output$results)
+        output <- tmp_output
+      }
+    }
+
+    output <- output$results
+    # some tables contain lists, flatten them first or bind_rows() will error
+    for(i in seq_along(results)){
+      for(ii in names(results[[i]])){
+        if(is.list(results[[i]][[ii]])){
+          if(!length(results[[i]][[ii]]))
+            results[[i]][[ii]] <- NA
+          else
+            results[[i]][[ii]] <- paste0(unlist(results[[i]][[ii]]), ",", collapse="")}
+      }
+    }
+    # bind the results into a dataframe
+    results <- bind_rows(results)
+
+    # select only url and id if detail is set to "id"
+    if(detail == "id"){
+      if(length(results) > 1)
+        results <- results %>% dplyr::select(url) %>% dplyr::mutate(id = basename(url))
+    }
+
+  if(length(results) == 0)
+    message("No results were returned")
+  results
 
 }
