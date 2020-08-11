e5e01b393fdab60eb7ff99e5e5a4131b1e620c33
Ryan J Field <ryan@ryanfield.me>
2020-08-12 00:44:13 +0100

    add test for get_hash() #11 add nicer error if file does not exist

--- R/get_hash.R
+++ R/get_hash.R
@@ -1,11 +1,14 @@
 #' get_hash
+#' Returns the SHA1 hash of a given file
 #'
 #' @param filename filename
 #'
 #' @export
 #'
 get_hash <- function(filename) {
-    file(filename) %>%
-    openssl::sha1() %>%
-    as.character()
+  if(!file.exists(filename))
+    stop(paste0("File ", filename, " does not exist"))
+  file(filename) %>%
+  openssl::sha1() %>%
+  as.character()
 }
