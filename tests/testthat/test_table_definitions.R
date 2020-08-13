008eefac04d327d09b6106bd6cf4c34d7a5e0ea5
Ryan J Field <ryan@ryanfield.me>
2020-08-13 12:07:13 +0100

    actually use the token...

--- tests/testthat/test_table_definitions.R
+++ tests/testthat/test_table_definitions.R
@@ -3,6 +3,7 @@ context("Test table definitions and validation methods")
 # get the token
 token <- Sys.getenv("SCRC_API_TOKEN")
 
+
 #get all tables
 tables <- get_tables()
 
@@ -12,5 +13,5 @@ tables_sample <- tables[sample(length(tables), 3)]
 
 test_that("Field are returned", {
   for(i in seq_along(tables_sample))
-    expect_true(is.character(tables_sample[i]))
+    expect_true(is.character(get_fields(tables_sample[i], token)))
 })
