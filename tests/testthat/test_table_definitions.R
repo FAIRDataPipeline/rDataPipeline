9b006d4e8941f3befcb27bc04068c844505671cd
Ryan J Field <ryan@ryanfield.me>
2020-08-13 14:37:36 +0100

    Fix issue with table_definitions and get_table_required()
    Add more tests to test_table_definitions #11

--- tests/testthat/test_table_definitions.R
+++ tests/testthat/test_table_definitions.R
@@ -3,15 +3,35 @@ context("Test table definitions and validation methods")
 # get the token
 token <- Sys.getenv("SCRC_API_TOKEN")
 
-
 #get all tables
 tables <- get_tables()
+table_unknown <- "unknown"
 
 #use a sample of tables to prevent api from erroring
 set.seed(123)
 tables_sample <- tables[sample(length(tables), 3)]
 
-test_that("Field are returned", {
-  for(i in seq_along(tables_sample))
+test_that("Fields are returned by get_fields", {
+  for(i in seq_along(tables_sample)){
     expect_true(is.character(get_fields(tables_sample[i], token)))
+    expect_true(is.character(get_table_writable(tables_sample[i], token)))
+    expect_true(is.character(get_table_readable(tables_sample[i], token)))
+    expect_true(is.character(get_table_optional(tables_sample[i], token)))
+    expect_true(is.character(get_table_required(tables_sample[i], token)))
+    #default behavior if filter fields is not recognized
+    expect_false(is.character(get_fields(tables_sample[i], token, "default")))
+  }
+
+})
+
+test_that("Users and groups behave correctly with is_queryable()",{
+  expect_false(check_query("users", list(URL = "1")))
+  expect_false(check_query("groups", list(URL = "1")))
+})
+
+test_that("Errors occur if table does not exist", {
+  expect_error(get_table_writable(unknown_table, token))
+  expect_error(get_table_readable(unknown_table, token))
+  expect_error(get_table_optional(unknown_table, token))
+  expect_error(get_table_required(unknown_table, token))
 })
