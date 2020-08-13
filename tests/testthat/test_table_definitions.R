98f2d94e7ae942601a926a1c28247cf7101595c4
Ryan J Field <ryan@ryanfield.me>
2020-08-13 14:52:46 +0100

    Try for 100% coverage on table_definitions

--- tests/testthat/test_table_definitions.R
+++ tests/testthat/test_table_definitions.R
@@ -5,7 +5,7 @@ token <- Sys.getenv("SCRC_API_TOKEN")
 
 #get all tables
 tables <- get_tables()
-table_unknown <- "unknown"
+unknown_table <- "unknown"
 
 #use a sample of tables to prevent api from erroring
 set.seed(123)
@@ -18,6 +18,7 @@ test_that("Fields are returned by get_fields", {
     expect_true(is.character(get_table_readable(tables_sample[i], token)))
     expect_true(is.character(get_table_optional(tables_sample[i], token)))
     expect_true(is.character(get_table_required(tables_sample[i], token)))
+    expect_true(is.character(get_fields(tables_sample[i], token, "read_only")))
     #default behavior if filter fields is not recognized
     expect_false(is.character(get_fields(tables_sample[i], token, "default")))
   }
