b7208c55e0ff9b1225bd0f6d42cd44baa9402724
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-19 09:57:13 +0100

    Rename file to match first function called by zzz. Undo paste0() on URL as this was causing a slow down; file.path() makes more sense anyway.

---
+++ R/get_startup_message.R
@@ -0,0 +1,57 @@
+#' Get startup message
+#'
+#' @return returns the startup message
+#'
+get_startup_message <- function(){
+  if(! is_current_version()){
+    return(paste("Warning: Your package version is out of date please update\n",
+                 "Git Version: ", get_remote_package_version(),
+                 " Local Version: ", get_loaded_package_version()))}
+  else{
+    if (crayon::has_color())
+      return(crayon::green(paste("Version: ", get_loaded_package_version(),
+                                 " Your package is up to date")))
+    else
+      return(paste("Version: ", get_loaded_package_version(),
+                   " Your package is up to date"))
+  }
+}
+
+#' Get loaded Package Version
+#'
+#' @return Returns the current package version
+#'
+get_loaded_package_version <- function(){
+  return(utils::packageVersion("SCRCdataAPI"))
+}
+
+#' Get github Package Version
+#'
+#' @return Returns the current package version
+#'
+get_remote_package_version <- function(){
+  description <- utils::read.delim(
+    file.path("https://raw.githubusercontent.com", "ScottishCovidResponse",
+              "SCRCdataAPI", "master", "DESCRIPTION"), sep = ":",
+    header = FALSE, row.names = 1)
+  if(any(row.names(description) == "Version"))
+    return(gsub(" ", "", description['Version',]))
+  else
+    stop("Something went wrong")
+}
+
+#' Check if Package is current version
+#'
+#' @return Returns true if the loaded package is the same version as the github
+#' package
+#'
+is_current_version <- function()
+{
+  if(as.numeric_version(get_loaded_package_version()) ==
+     as.numeric_version(get_remote_package_version()))
+    return(TRUE)
+  else
+    return(FALSE)
+}
+
+
