340fe6ae76a067336464f75df114b0cd607371ac
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-19 15:54:51 +0100

    check git is installed

--- R/get_github_hash.R
+++ R/get_github_hash.R
@@ -1,12 +1,13 @@
 #' get_github_hash
-#' This function assumes git is installed and located in the
-#' System PATH
+#'
+#' This function assumes git is installed and located in the System PATH
 #'
 #' @param repo e.g. "ScottishCovidResponse/SCRCdata"
 #'
-#' @export
-#'
 get_github_hash <- function(repo) {
+  if(system("git --version"))
+    stop("git must be installed and located in the system path for this function to work")
+
   tmp <- system(paste0("git ls-remote -h https://github.com/", repo, ".git"),
          intern = TRUE)
   gsub("\trefs/heads/master", "", tmp)
