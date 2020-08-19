1a921310c54a846a1b5127de913bda3428d3211b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-19 10:01:24 +0100

    This function is used by attach_issue()

---
+++ R/patch_data.R
@@ -0,0 +1,27 @@
+#' patch_data
+#'
+#' @param url url
+#' @param data data
+#' @param key key
+#'
+patch_data <- function(url,
+                       data,
+                       key) {
+
+  h <- c(Authorization = paste("token", key))
+
+  result <- httr::PATCH(url,
+                        body = jsonlite::toJSON(data, pretty = T,
+                                                auto_unbox = T,
+                                                force = T),
+                        httr::content_type('application/json'),
+                        httr::add_headers(.headers = h),
+                        verbose())
+
+  tmp <- result %>%
+    httr::content("text") %>%
+    jsonlite::fromJSON(simplifyVector = FALSE)
+
+  if(result$status != 200)
+      stop("Adding new data returned non-200 status code:", tmp)
+}
