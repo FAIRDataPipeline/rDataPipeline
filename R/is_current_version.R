6f328a8638f474cd82ce79fd57b2c40c2e27b55b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-08-19 15:57:13 +0100

    take arguments from get_loaded_package_version() and get_remote_package_version()

--- R/is_current_version.R
+++ R/is_current_version.R
@@ -3,10 +3,11 @@
 #' @return Returns true if the loaded package is the same version as the github
 #' package
 #'
-is_current_version <- function()
+is_current_version <- function(repo = "ScottishCovidResponse/SCRCdataAPI",
+                               package = "SCRCdataAPI")
 {
-  if(as.numeric_version(get_loaded_package_version()) ==
-     as.numeric_version(get_remote_package_version()))
+  if(as.numeric_version(get_loaded_package_version(package)) ==
+     as.numeric_version(get_remote_package_version(repo)))
     return(TRUE)
   else
     return(FALSE)
