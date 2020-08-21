787a46191935a061de30075df009a0db6c4b1c46
ciaranmcmonagle <c.mcmonagle.1@research.gla.ac.uk>
2020-08-21 14:21:38 +0100

    fix file names

--- R/check_for_hdf.R
+++ R/check_for_hdf.R
@@ -1,15 +1,14 @@
 check_for_hdf5<-function(filename, component){
   # Does file exist?
-  if(file.exists(h5filename)==TRUE){
+  if(file.exists(filename)==TRUE){
     # If yes:
     # Does the component exist?
-  file.h5 <- H5File$new(filename, mode = "r")
-  check=any(grepl(component,file.h5$ls(recursive=TRUE)$name))
-  file.h5$close_all()
-  }else if(file.exists(h5filename)==FALSE){
+    file.h5 <- H5File$new(filename, mode = "r")
+    check=any(grepl(component,file.h5$ls(recursive=TRUE)$name))
+    file.h5$close_all()
+  }else if(file.exists(filename)==FALSE){
     # If no:
     check=FALSE
   }
   check
 }
-
