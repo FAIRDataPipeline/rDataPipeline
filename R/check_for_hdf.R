c3ddcb262a6188a236299b8d387d0612dd338095
Ryan J Field <ryan@ryanfield.me>
2020-08-24 11:47:28 +0100

    Refactor check_for_hdf5 and add documentation add test for check_for_hdf5 #11

--- R/check_for_hdf.R
+++ R/check_for_hdf.R
@@ -1,14 +1,25 @@
+#' check_for_hdf5
+#' internal function to check if a component is in a given hdf5 file
+#'
+#' @param filename file path to the hdf5
+#' @param component component name to check for
+#'
+#'
 check_for_hdf5<-function(filename, component){
-  # Does file exist?
+
+  #Default value for check
+  check <- FALSE
+
+  if(is.null(filename) | is.na(filename))  {
+   stop("filename should not be NULL or NA")
+  }
   if(file.exists(filename)==TRUE){
     # If yes:
     # Does the component exist?
     file.h5 <- H5File$new(filename, mode = "r")
-    check=any(grepl(component,file.h5$ls(recursive=TRUE)$name))
+    check <- any(grepl(component,file.h5$ls(recursive=TRUE)$name))
     file.h5$close_all()
-  }else if(file.exists(filename)==FALSE){
-    # If no:
-    check=FALSE
-  }
+    }
+
   check
 }
