c8f2aad0b3b5522c5d3612bf25ad0cb97c63e761
Ryan J Field <ryan@ryanfield.me>
2020-08-24 12:41:40 +0100

    Add error to get fields if the table is groups or uses, fix test_table_definitions

--- tests/testthat/test_table_definitions.R
+++ tests/testthat/test_table_definitions.R
@@ -7,25 +7,32 @@ token <- Sys.getenv("SCRC_API_TOKEN")
 tables <- get_tables()
 unknown_table <- "unknown"
 
-# #use a sample of tables to prevent api from erroring
-# set.seed(123)
-# tables_sample <- tables[sample(length(tables), 3)]
-#
-# test_that("Fields are returned by get_fields", {
-#   for(i in seq_along(tables_sample)){
-#     expect_true(is.character(get_fields(tables_sample[i], token)))
-#     expect_true(is.character(get_table_writable(tables_sample[i], token)))
-#     expect_true(is.character(get_table_readable(tables_sample[i], token)))
-#     expect_true(is.character(get_table_optional(tables_sample[i], token)))
-#     expect_true(is.character(get_table_required(tables_sample[i], token)))
-#     expect_true(is.character(get_fields(tables_sample[i], token, "read_only")))
-#     #default behavior if filter fields is not recognized
-#     expect_false(is.character(get_fields(tables_sample[i], token, "default")))
-#   }
-# })
+sleep_time <- 0.1
 
 
-
+test_that("Fields are returned by get_fields", {
+  for(i in seq_along(tables)){
+    if(tables[i] == "users" | tables[i] == "groups")
+      expect_error(get_fields(tables[i], token))
+    else{
+    expect_true(is.character(get_fields(tables[i], token)))
+    Sys.sleep(sleep_time)
+    expect_true(is.character(get_table_writable(tables[i], token)))
+    Sys.sleep(sleep_time)
+    expect_true(is.character(get_table_readable(tables[i], token)))
+    Sys.sleep(sleep_time)
+    expect_true(is.character(get_table_optional(tables[i], token)))
+    Sys.sleep(sleep_time)
+    expect_true(is.character(get_table_required(tables[i], token)))
+    Sys.sleep(sleep_time)
+    expect_true(is.character(get_fields(tables[i], token, "read_only")))
+    Sys.sleep(sleep_time)
+    #default behavior if filter fields is not recognized
+    expect_false(is.character(get_fields(tables[i], token, "default")))
+    Sys.sleep(sleep_time)
+    }
+  }
+})
 
 test_that("Users and groups behave correctly with is_queryable()",{
   expect_false(check_query("users", list(URL = "1")))
