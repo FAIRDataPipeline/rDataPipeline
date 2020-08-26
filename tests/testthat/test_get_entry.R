6aa83ad5716b4f9b1f212f5f40f9e0e92b705e2e
Ryan J Field <ryan@ryanfield.me>
2020-08-26 11:01:39 +0100

    Add pauses to stop API timeout errors

--- tests/testthat/test_get_entry.R
+++ tests/testthat/test_get_entry.R
@@ -2,11 +2,14 @@ context("Checking get entry")
 
 key <- Sys.getenv("SCRC_API_TOKEN")
 
+sleep_time <- 0.5
+
 formatted_date <- format(Sys.time(), "%d%m%y%H%M%S")
 UID <- paste0("TEST OBJECT ", formatted_date)
 object_id <- post_data("object", data = list(description = UID), key)
 object_id <- unlist(clean_query(object_id))
 description <- UID
+Sys.sleep(sleep_time)
 
 test_that("Check Test object Exists", {
   expect_silent(
@@ -15,10 +18,12 @@ test_that("Check Test object Exists", {
     )
 })
 
+Sys.sleep(sleep_time)
 test_that("Unknown Table causes and error", {
   expect_error(get_entry("unknown", list()))
 })
 
+Sys.sleep(sleep_time)
 test_that("invalid query causes and error", {
   expect_error(get_entry("object", list(url="")))
   expect_error(get_entry("object", "query"))
@@ -30,17 +35,22 @@ test_that("invalid query causes and error", {
 })
 
 test_that("query = \"\" produces a warning", {
+  Sys.sleep(sleep_time)
   expect_warning(get_entry("object", "" ))
 })
 
+Sys.sleep(sleep_time)
 test_that("multiple matches returns a list of more than one object", {
   post_data("object", data = list(description = description), key)
+  Sys.sleep(sleep_time)
   expect_true(length(get_entry("object", list(description = description))) > 1)
 })
 
+Sys.sleep(sleep_time)
 test_that("Blank query returns list of objects", {
   expect_silent(expect_true(is.list(get_entry("object", list()))))
 })
+Sys.sleep(sleep_time)
 
 test_that("Check Objects have correct fields", {
   object <- get_entry("object", query = list(description=description))
