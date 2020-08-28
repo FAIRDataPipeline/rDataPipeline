231cae81969c50c4269cfea5aae373ae248e29d1
Ryan J Field <ryan@ryanfield.me>
2020-08-28 15:16:46 +0100

    rename and add more tests to test_validate_post_data

--- tests/testthat/test_validate_post_data.R
+++ tests/testthat/test_validate_post_data.R
@@ -8,20 +8,43 @@ key <- Sys.getenv("SCRC_API_TOKEN")
 tables <- get_tables()
 unknown_table <- "unknown"
 
+test_that("incorrect tables produce and error", {
+  expect_error(validate_post_data(unknown_table, data = list(), key))
+  expect_error(validate_post_data(NULL, data = list(), key))
+})
+
 test_that("validate_post_date works with all tables",{
   for(i in seq_along(tables)){
     if(tables[i] == "users" | tables[i] == "groups")
       expect_error(validate_post_data(tables[i], data = list("username = test"), token))
     else{
       Sys.sleep(sleep_time)
-      data = list()
+      data <- list()
+      data2 <- list()
+      data3 <- list()
       table.writable <- get_table_writable(tables[i], key)
+      Sys.sleep(sleep_time)
+      table.required <- get_table_required(tables[i], key)
       for(field in seq_along(table.writable))
         data[table.writable[field]] <- "test"
+
+      if(length(table.required) > 1)
+          data2[table.required[2]] <- "test"
+      else
+        data2 <- list(unknown = "unknown")
+
+      data3 <- c(data, unknown = "test")
 
       expect_silent(validate_post_data(tables[i], data, key))
       Sys.sleep(sleep_time)
       expect_error(validate_post_data(tables[i], data=list(unknown = "unknown"), key))
-      Sys.sleep(sleep_time)#
+      Sys.sleep(sleep_time)
+
+      expect_error(validate_post_data(tables[i], data2, key))
+      Sys.sleep(sleep_time)
+
+      expect_error(validate_post_data(tables[i], data3, key))
+      Sys.sleep(sleep_time)
+
     }}
 })
