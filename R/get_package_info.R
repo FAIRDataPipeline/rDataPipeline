f53c1ab1d982ab4f9e4f039ae7d799b90c8a784c
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-09-09 14:23:28 +0100

    edit tests and documentation

--- R/get_package_info.R
+++ R/get_package_info.R
@@ -6,9 +6,11 @@
 #' as a package, checks are made to ensure remote and local versions of the
 #' package have the same version number.
 #'
-#' @param repo *e.g.* "ScottishCovidResponse/SCRCdata"
-#' @param script_path *e.g.* "scotgov_deaths.R"
-#' @param package (optional) *e.g.* "SCRCdata"
+#' @param repo a \code{string} specifying the github username/repository
+#' @param script_path a \code{string} specifying the submission script filename
+#' and extension
+#' @param package (optional) a \code{string} specifying the name of the package
+#' containing the submission script
 #'
 #' @return Returns a list comprising:
 #' * the github storage root name: "github"
@@ -19,42 +21,41 @@
 #'
 #' @export
 #'
+#' @examples
+#' \donttest{
+#' \dontrun{
+#' get_package_info(repo = "ScottishCovidResponse/SCRCdata",
+#' script_path = "scotgov_deaths.R",
+#' package = "SCRCdata")
+#' }}
+#'
 get_package_info <- function(repo, script_path, package) {
-  # Check repository contains DESCRIPTION file
-  tryCatch(
-    {
-      readLines(paste("https://raw.githubusercontent.com", repo, "master",
-                          "DESCRIPTION", sep = "/"))
-    }, warning = function(warn) {
-      stop(paste(repo, "does not contain a DESCRIPTION file, please create one"))
-    }
-  )
+  # List files in repository
+  filelist <- github_files(repo)
 
-  # Check script exists in the repository
-  tryCatch(
-    {
-      readLines(paste("https://raw.githubusercontent.com", repo, "master",
-                          script_path, sep = "/"), warn = FALSE)
-    }, warning = function(warn) {
+  # If repository contains a DESCRIPTION file, assume it's an R package
+  if(any(grepl("DESCRIPTION", filelist, fixed = TRUE))) {
+
+    # Check script exists in the repository
+    if(!any(grepl(paste0(script_path, "$"), filelist)))
       stop(paste(script_path, "does not exist within", repo))
+
+    # If a package does not exist, the GitHub repo should at least contain a
+    # DESCRIPTION file
+    if(missing(package)) {
+      message(paste("The current githash has been downloaded from your repository.",
+                    "If you have not yet pushed your processing script to this",
+                    "repository, please PUSH IT NOW and run this function again."))
+
+    } else {
+      # If a package does exist, check that the version number in the GitHub repo
+      # DESCRIPTION file matches the currently installed package version number
+      if(!is_current_version(repo, package))
+        stop(paste("Your package version is out of date please update it",
+                   "and run this function again\n",
+                   "Git Version: ", get_remote_package_version(repo),
+                   "\n Local Version: ", get_loaded_package_version(package)))
     }
-  )
-
-  # If a package does not exist, the GitHub repo should at least contain a
-  # DESCRIPTION file
-  if(missing(package)) {
-    message(paste("The current githash has been downloaded from your repository.",
-                  "If you have not yet pushed your processing script to this",
-                  "repository, please PUSH IT NOW and run this function again."))
-
-  } else {
-    # If a package does exist, check that the version number in the GitHub repo
-    # DESCRIPTION file matches the currently installed package version number
-    if(!is_current_version(repo, package))
-      stop(paste("Your package version is out of date please update it",
-                 "and run this function again\n",
-                 "Git Version: ", get_remote_package_version(repo),
-                 "\n Local Version: ", get_loaded_package_version(package)))
   }
 
   # Get the version number from the GitHub repo DESCRIPTION file
