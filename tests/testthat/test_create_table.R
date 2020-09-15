e48767819e45681048d7ae8d03556298c62de700
Ryan Field <ryan@ryanfield.me>
2020-09-15 22:55:17 +0100

    create_estimate fix, add tests to create_table

--- tests/testthat/test_create_table.R
+++ tests/testthat/test_create_table.R
@@ -4,8 +4,12 @@ library(SCRCdataAPI)
 context("Testing create_table() on a basic table")
 
 filename <- "test_table.h5"
+filename_2 <- "test_table2.h5"
+filename_3 <- "test_table3.h5"
+filepath <- paste0("data-raw/", filename_2)
 df <- data.frame(a = 1:2, b = 3:4)
 component <- "level"
+component_2 <- "level/level_2"
 
 test_that("incorrect file name throws an error", {
   testthat::expect_error(create_table(filename = "test_table",
@@ -59,6 +63,26 @@ test_that("function behaves as it should with multiple classes", {
 
 file.remove(filename)
 
+test_that("directory is created when supplying path",{
+    create_table(filename = filename_2,
+                 path = "data-raw",
+                 component = component_2,
+                 df = df)
+    testthat::expect_true(hdf5r::is.h5file(filepath))
+})
+
+file.remove(filepath)
+
+test_that("create tables works with rownames and units",{
+  create_table(filename = filename_3,
+               row_names = c("row_1", "row_2"),
+               column_units = c("days", "months"),
+               component = component,
+               df = df)
+  testthat::expect_true(hdf5r::is.h5file(filename_3))
+})
+
+file.remove(filename_3)
 
 
 
