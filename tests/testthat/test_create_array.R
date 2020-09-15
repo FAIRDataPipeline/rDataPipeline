75de65e7137f8bbc673ad77ade3d029485c0b165
Ryan Field <ryan@ryanfield.me>
2020-09-15 22:20:33 +0100

    add test for create_array with file path

--- tests/testthat/test_create_array.R
+++ tests/testthat/test_create_array.R
@@ -4,6 +4,8 @@ context("Testing create_array()")
 
 
 filename <- "test_array.h5"
+filename_1 <- "test_array_1.h5"
+filepath <- paste0("test/", filename_1)
 component <- "level"
 df <- data.frame(a = 1:2, b = 3:4)
 rownames(df) <- 1:2
@@ -47,12 +49,23 @@ test_that("incorrect dimension_names length throws error", {
                                         colvalue = colnames(df)))
   )
 
+
+
   testthat::expect_error(
     create_array(filename = filename,
                  component = component,
                  array = as.matrix(df),
                  dimension_names = list(rowvalue = rownames(df),
                                         colvalue = 1))
+  )
+
+  testthat::expect_error(
+    create_array(filename = filename,
+                 component = component,
+                 array = as.matrix(df),
+                 dimension_names = list(rowvalue = rownames(df),
+                                        colvalue = colnames(df),
+                                        othervalue = colnames(df)))
   )
 })
 
@@ -65,6 +78,16 @@ test_that(".h5 file is generated", {
   testthat::expect_true(hdf5r::is.h5file(filename))
 })
 
+test_that(".h5 file is generated in new directory", {
+  create_array(filename = filename_1,
+               path = "test",
+               component = component,
+               array = as.matrix(df),
+               dimension_names = list(rowvalue = rownames(df),
+                                      colvalue = colnames(df)))
+  testthat::expect_true(hdf5r::is.h5file(filepath))
+})
+
 test_that("component name is level", {
   file.h5 <- H5File$new(filename, mode = "r")
   testthat::expect_equal(names(file.h5), component)
@@ -75,3 +98,6 @@ test_that("component name is level", {
 
 # Remove test file
 file.remove(filename)
+file.remove(filepath)
+
+unlink("test", recursive = TRUE)
