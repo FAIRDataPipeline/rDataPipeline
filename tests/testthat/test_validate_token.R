cc2b61875fdd98648c2ca50202850bbc778b0db2
Ryan J Field <ryan@ryanfield.me>
2020-09-23 14:37:17 +0100

    Complete token validation and test

--- tests/testthat/test_validate_token.R
+++ tests/testthat/test_validate_token.R
@@ -1,17 +1,31 @@
-context("Testing validate_token")
+context("Testing key validation")
 
-# get the token
-key <- Sys.getenv("SCRC_API_TOKEN")
+correct_key <- Sys.getenv("SCRC_API_TOKEN")
 
-test_that("validate token produces errors when invalid token supplied", {
+key_num <- paste(rep(0:9, 4), collapse = "", sep="")
+key_alpha <- paste(sample(letters, 40, TRUE), collapse = "", sep="")
+
+key_alphanum <- paste(sample(letters, 20, TRUE), rep(0:9, 4), collapse = "", sep="")
+
+key_non_alpha <- paste(sample(letters, 20, TRUE), "/", collapse = "", sep="")
+
+key_non_alpha_2 <- paste(sample(letters, 38, TRUE), collapse = "", sep="") %>%
+  paste0("\n")
+
+test_that("validate token fails with invalid keys", {
+  expect_error(validate_token())
   expect_error(validate_token(NULL))
   expect_error(validate_token(""))
-  expect_error(validate_token(5))
-  expect_error(validate_token())
-
-
+  expect_error(validate_token(as.character(Inf)))
+  expect_error(validate_token(as.character(rep(0:9, 4))))
+  expect_error(validate_token(40))
+  expect_error(validate_token(list()))
+  expect_error(validate_token(key_non_alpha))
+  expect_error(validate_token(key_non_alpha_2))
+  expect_error(validate_token(key_num))
+  expect_error(validate_token(key_alpha))
 })
 
-test_that("validate_token works with a token", {
-  expect_true(is.character(validate_token(key)))
+test_that("Test API TOKEN works with validate_token", {
+  expect_equal(correct_key, validate_token(correct_key))
 })
