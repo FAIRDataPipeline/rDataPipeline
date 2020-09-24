e499ad468e8b59669ebd1742a9c54855f3d3886a
Ryan J Field <ryan@ryanfield.me>
2020-09-24 09:37:19 +0100

    Fix documentation errors

--- R/grid_intersection.R
+++ R/grid_intersection.R
@@ -2,13 +2,19 @@
 #'
 #' @param shapefile shapefile
 #' @param gridsize gridsize
+#' @param grid_shp gridshape
 #'
 #' @export
 #'
 grid_intersection <- function(shapefile,
                               gridsize,
                               grid_shp = NA) {
-  
+
+  # Don't know if you need both grid_shp and grids_shp but this line is to stop
+  # the note on r cmd CHECK
+  grids_shp <- NA
+
+
   # Check units of shapefile: assuming metres below
   sh_unit <- sf::st_crs(shapefile, parameters = TRUE)$units_gdal
   assertthat::assert_that(sh_unit == "metre",
@@ -18,7 +24,7 @@ grid_intersection <- function(shapefile,
     n <- gridsize*1000  # Assume gridsize given in km
     grids <- sf::st_make_grid(sf::st_as_sfc(sf::st_bbox(shapefile)),
                               cellsize = c(n, n))
-    
+
     width <- sf::st_bbox(shapefile)$xmax - sf::st_bbox(shapefile)$xmin
     height <- sf::st_bbox(shapefile)$ymax - sf::st_bbox(shapefile)$ymin
     num_columns <- ceiling(width / n)
@@ -27,9 +33,9 @@ grid_intersection <- function(shapefile,
     grid_matrix <- strsplit(grid_labels, "-") %>% lapply(as.numeric)
     grid_matrix <- do.call(rbind.data.frame, grid_matrix)
     colnames(grid_matrix) <- c("x", "y")
-    
+
     grids <- sf::st_sf(grids, grid_id = grid_labels)
-    
+
     assertthat::assert_that(max(grid_matrix$x) == num_columns)
     assertthat::assert_that(max(grid_matrix$y) == num_rows)
   }else if(any(grepl("sf", class(grids_shp))==TRUE)){
@@ -41,9 +47,9 @@ grid_intersection <- function(shapefile,
   }else{
     stop("grids_shp must be either not present or an simple features object")
   }
-  
+
   subdivisions <- sf::st_intersection(grids, shapefile)
-  
+
   # Remove cells which intersect simply because they touch the shapefile
   # - These are reduced to lines or points in the intersection
   is_polygon <- sapply(subdivisions$grids, function(x)
@@ -63,14 +69,14 @@ grid_intersection <- function(shapefile,
     left_join(datazone_area, by = "AREAcode")
   subdivision_area$difference = subdivision_area$subd_area - subdivision_area$dz_area
   assertthat::assert_that(all(round(as.numeric(subdivision_area$difference)) == 0))
-  
+
   # Adjust grid labels to match
   ind <- which(grid_labels %in% unique(subdivisions$grid_id))
   grid_matrix <- grid_matrix[ind,]
-  
+
   tmp <- sort(apply(grid_matrix, 1, function(x) paste(x[1], x[2], sep = "-")))
   assertthat::assert_that(all(tmp == sort(unique(subdivisions$grid_id))))
-  
+
   # Use grid to subdivide shapefile
   list(subdivisions = subdivisions,
        grid_matrix = grid_matrix)
