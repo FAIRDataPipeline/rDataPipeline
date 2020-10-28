31c3a4736e0f1a585e6e1ab6498264136b89a187
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2020-10-28 12:41:12 +0000

    make this error

--- R/get_entry.R
+++ R/get_entry.R
@@ -6,21 +6,18 @@
 #'
 #' @export
 #'
+#' @examples
+#' get_entry("storage_root", list(name = "github"))
+#'
 get_entry <- function(table, query = list()) {
-  # to be Deprecated
-  if(is.character(query)){
-    if(query == ""){
-      warning("using query as \"\" is deprecated and will be removed")
-      query <- list()
-    }
-  }
+  # An empty string is an invalid query
+  if(is.character(query) && query == "") stop("not a valid query")
+  # Check whether table is valid
+  if(!check_table_exists(table)) stop(paste("table doesn't exist"))
+  # Check whether query is valid (for table)
+  if(!check_query(table, query)) stop("not a valid query for table")
 
-  if(!check_table_exists(table))
-    stop(paste("table ", table, " does not exist"))
-  if(!check_query(table, query))
-    stop("not a valid query for table")
-
-   out <- httr::GET(paste("https://data.scrc.uk/api", table, "", sep = "/"),
+  out <- httr::GET(paste("https://data.scrc.uk/api", table, "", sep = "/"),
                    query = query) %>%
     httr::content(as = "text", encoding = "UTF-8") %>%
     jsonlite::fromJSON(simplifyVector = FALSE)
