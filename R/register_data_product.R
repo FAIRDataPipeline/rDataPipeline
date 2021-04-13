d122f50e39d0c3c92b2ea5a0ed65ee3a283403da
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-13 11:57:47 +0100

    placeholders

---
+++ R/register_data_product.R
@@ -0,0 +1,11 @@
+#' register_data_product
+#'
+#' @param register_this metadata
+#' @param datastore default local data storage location
+#' @param namespace
+#'
+register_data_product <- function(register_this,
+                                  datastore,
+                                  namespace) {
+
+}
