ae4a6406089ef8ed09e1814ac1ac966b636b9b7f
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-13 11:55:58 +0100

    new function to get file location from local storage

---
+++ R/read_link.R
@@ -0,0 +1,20 @@
+#' read_link
+#'
+#' @param handle list
+#' @param alias string
+#'
+#' @export
+#'
+read_link <- function(handle, alias) {
+  # Get entries
+  url <- unname(handle$inputs[alias])
+  run_server()
+  external_object <- get_entity(url)
+  object <- get_entity(external_object$object)
+  storage_location <- get_entity(object$storage_location)
+  storage_root <- get_entity(storage_location$storage_root)
+  stop_server()
+
+  # Return storage location
+  invisible(file.path(storage_root$root, storage_location$path))
+}
