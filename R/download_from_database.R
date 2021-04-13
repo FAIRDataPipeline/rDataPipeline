f79911bc08880619543c6254633ef078999390b0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-13 12:01:24 +0100

    tidy up

--- R/download_from_database.R
+++ R/download_from_database.R
@@ -64,13 +64,13 @@ download_from_database <- function(source_root,
                                    path,
                                    filename,
                                    overwrite = FALSE) {
-  # Generate directory structure
-  if(!file.exists(path)) dir.create(path, recursive = TRUE)
-  if(missing(path)) path <- ""
+    # Generate directory structure
+    if(!file.exists(path)) dir.create(path, recursive = TRUE)
+    if(missing(path)) path <- ""
 
-  # Download file
-  httr::GET(paste0(source_root,
-                   utils::URLencode(source_path, reserved = TRUE)),
-            httr::content_type("text/csv"),
-            httr::write_disk(file.path(path, filename), overwrite = overwrite))
+    # Download file
+    httr::GET(paste0(source_root,
+                     utils::URLencode(source_path, reserved = TRUE)),
+              httr::content_type("text/csv"),
+              httr::write_disk(file.path(path, filename), overwrite = overwrite))
 }
