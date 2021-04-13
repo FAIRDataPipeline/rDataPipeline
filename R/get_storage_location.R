2c5719922fd0a4345d96cac80ac72d5a2db311c0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-13 11:52:51 +0100

    get storage location from url rather than id number

--- R/get_storage_location.R
+++ R/get_storage_location.R
@@ -1,9 +1,8 @@
-#' Get storage location from id
+#' Get storage location from url
 #'
 #' Get storage location entry
 #'
-#' @param location_id an \code{integer} specifying the id of an entry in the
-#' storage_location table
+#' @param location the url of an entry in the storage_location table
 #'
 #' @return Returns a \code{list} of fields associated with the specified entry
 #' @keywords internal
@@ -13,6 +12,6 @@
 #' get_storage_location(10404)
 #' }
 #'
-get_storage_location <- function(location_id){
-  get_entity("storage_location", location_id)
+get_storage_location <- function(location){
+  get_entity("storage_location", basename(location))
 }
