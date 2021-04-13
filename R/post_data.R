6dafed24e98864dbe0e35a5c4bb0d1f22909dc8c
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-13 11:50:15 +0100

    comment out validation for the time being (it will eventually move to `initialise()`)

--- R/post_data.R
+++ R/post_data.R
@@ -14,15 +14,15 @@ post_data <- function(table, data, ...) {
   key <- readLines(file.path("~", ".scrc", "TOKEN.txt"))
 
   # If the skip_table_validation argument has been input, skip validate_table()
-  dots <- list(...)
-  if(any(names(dots) %in% "skip_table_validation"))
-    skip_table_validation <- dots[["skip_table_validation"]] else
-      skip_table_validation <- FALSE
-  if(!skip_table_validation)
-    table <- validate_table(table, key)
+  # dots <- list(...)
+  # if(any(names(dots) %in% "skip_table_validation"))
+  #   skip_table_validation <- dots[["skip_table_validation"]] else
+  #     skip_table_validation <- FALSE
+  # if(!skip_table_validation)
+  #   table <- validate_table(table, key)
 
   # Validate fields
-  data <- validate_fields(table, data, key)
+  # data <- validate_fields(table, data, key)
 
   h <- c(Authorization = paste("token", key))
 
