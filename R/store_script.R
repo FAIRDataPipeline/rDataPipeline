574bb9cc2b721abbb9678c0f68d79f34c0808c97
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-15 16:17:10 +0100

    tidy up

--- R/store_script.R
+++ R/store_script.R
@@ -1,26 +1,19 @@
 #' store_script
 #'
-#' @param yaml config.yaml
-#' @param time time
+#' @param script string
+#' @param path filename
 #'
 #' @export
 #'
-store_script <- function(yaml, time) {
-  x <- yaml$run_metadata
-  # If local data store doesn't exist, create it
-  localstore <- x$default_data_store
-  if (grepl("/$", localstore))
-    localstore <- gsub("/$", "", localstore)
-
+store_script <- function(script, path) {
   # If scripts directory doesn't exist, create it
-  scriptdir <- file.path(localstore, "scripts")
-  if (!file.exists(scriptdir))
-    dir.create(scriptdir)
+  directory <- dirname(path)
+  if (!file.exists(directory))
+    dir.create(directory)
 
-  # Write .sh file (increment file name if it exists - why would it though?)
-  filename <- paste0(time, ".sh")
-  filename <- increment_filename(scriptdir, filename)
+  # Write script file (increment file name if it exists - why would it though?)
+  path <- increment_filename(path)
 
-  cat(x$script, file = file.path(scriptdir, filename))
-  invisible(file.path("scripts", filename))
+  cat(script, file = path)
+  invisible(path)
 }
