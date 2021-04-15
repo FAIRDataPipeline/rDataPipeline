bba6ad60fcaae651e30c2472f241493ba45395b3
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-15 16:15:34 +0100

    add issues field

--- R/new_object.R
+++ R/new_object.R
@@ -4,9 +4,9 @@
 #'
 #' @param storage_location_id a \code{string} specifying the API URL of the
 #' associated \code{storage_location} table
-#' *e.g.* "https://data.scrc.uk/api/storage_location/411/"
 #' @param description (optional) a \code{string} containing a free text
-#' description of the \code{object_component}
+#' description of the \code{object}
+#' @param issues a \code{list} of Issues URLs to associate with this Object
 #'
 #' Note that the \code{object} table contains \code{issues} as an
 #' additional optional field. This is not included here. Instead use
@@ -18,9 +18,16 @@
 #' @export
 #'
 new_object <- function(storage_location_id,
-                       description = "") {
-
-  post_data(table = "object",
-            data = list(description = description,
-                        storage_location = storage_location_id))
+                       description = "",
+                       issues) {
+  if (missing(issues)) {
+    post_data(table = "object",
+              data = list(description = description,
+                          storage_location = storage_location_id))
+  } else {
+    post_data(table = "object",
+              data = list(description = description,
+                          storage_location = storage_location_id,
+                          issues = issues))
+  }
 }
