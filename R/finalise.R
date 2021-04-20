b38c0fb9126135752f857e86db466d00540c69c0
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-04-20 12:13:15 +0100

    attach issues

--- R/finalise.R
+++ R/finalise.R
@@ -6,25 +6,32 @@ finalise <- function(handle) {
 
   run_server()
 
-  # Add local data store root to the data registry
+  # Add local data store root to the data registry --------------------------
+
   datastore <- handle$yaml$run_metadata$default_data_store
   datastore_root_id <- new_storage_root(
     name = "localstore",
     root = datastore,
     accessibility = 0) # TODO
 
-  # upload data product metadata to the registry ----------------------------
+  # -------------------------------------------------------------------------
 
-  outputs <- list()
+  if (nrow(handle$outputs) != 0) {
 
-  if (!is.null(handle$outputs$dataproducts)) {
+    data_products <- unique(handle$outputs$dataproduct)
 
-    for (i in seq_along(handle$outputs$dataproducts)) {
-      dataproduct <- names(handle$outputs$dataproducts)[i]
-      path <- handle$outputs$dataproducts[[i]]$path
+    for (i in seq_along(data_products)) {
+      data_product <- data_products[i]
+      path <- handle$outputs %>%
+        dplyr::filter(dataproduct == data_product) %>%
+        dplyr::select(path) %>%
+        unique() %>%
+        unlist() %>%
+        unname()
       storage_location <- gsub(datastore, "", path)
 
-      # Rename file
+      # Rename file ---------------------------------------------------------
+
       hash <- get_file_hash(path)
       tmp_filename <- basename(path)
       extension <- strsplit(tmp_filename, split = "\\.")[[1]][2]
@@ -32,28 +39,87 @@ finalise <- function(handle) {
       new_path <- gsub(tmp_filename, new_filename, path)
       file.rename(path, new_path)
 
-      # Read description from config.yaml
+      # Read description from config.yaml ------------------------------------
+
       writes <- handle$yaml$write
-      index <- which(unlist(lapply(writes, function(x)
-        dataproduct == x$data_product)))
-      this_version <- writes[[index]]$version
-      description <- writes[[index]]$description
+      index_dp <- which(unlist(lapply(writes, function(x)
+        data_product == x$data_product)))
+      this_version <- writes[[index_dp]]$version
+      description <- writes[[index_dp]]$description
 
       namespace <- handle$yaml$run_metadata$default_output_namespace
       namespace_id <- new_namespace(name = namespace)
 
-      dataproduct_id <- upload_data_product(
-        storage_root_id = datastore_root_id,
-        name = dataproduct,
-        description = description,
-        processed_path = new_path,
-        product_path = storage_location,
-        version = this_version,
-        namespace_id = namespace_id)
-      outputs <- c(outputs, dataproduct_id)
-    }
+      # Record file location to data registry --------------------------------
 
+      storage_location_id <- new_storage_location(
+        path = storage_location,
+        hash = get_file_hash(new_path),
+        storage_root_id = datastore_root_id)
+
+      object_id <- new_object(storage_location_id = storage_location_id,
+                              description = description)
+
+      if(grepl(".h5$", new_path)) {
+        components <- get_components(new_path)
+      } else if(grepl(".toml$", new_path)) {
+        components <- data.frame(name = component_name)
+      } else {
+        stop("Why is your data product not a toml or an h5 file?")
+      }
+
+      # Record components in data registry -----------------------------------
+
+      for (j in seq_along(components)) {
+
+        component_id <- new_object_component(name = components[j],
+                                             object_id = object_id)
+
+        # Update handle
+        handle$write_component_id(data_product, components[j], component_id)
+
+        # Attach issues to component -----------------------------------------
+
+        this_dataproduct <- writes[[index_dp]]
+        index_ct <- which(components[j] == names(this_dataproduct$components))
+        this_component <- this_dataproduct$components[[index_ct]]
+
+        if (any("issues" %in% names(this_component))) {
+          issue <- writes[[index_dp]]$issues
+          severity <- writes[[index_dp]]$severity
+
+          issue_with_component(issue,
+                               severity,
+                               data_product,
+                               namespace,
+                               this_component,
+                               this_version)
+        }
+      }
+
+      # Add data product to data registry ------------------------------------
+
+      product_dataProductId <- new_data_product(name = data_product,
+                                                version = this_version,
+                                                object_id = object_id,
+                                                namespace_id = namespace_id)
+      handle$write_dataproduct_id(data_product, product_dataProductId)
+
+      # Attach issues to data product ----------------------------------------
+
+      if (any("issues" %in% names(writes[[index_dp]]))) {
+        issue <- writes[[index_dp]]$issues
+        severity <- writes[[index_dp]]$severity
+
+        issue_with_dataproduct(issue,
+                               severity,
+                               data_product,
+                               namespace,
+                               this_version)
+      }
+    }
   }
+
 
   # link objects together ---------------------------------------------------
 
@@ -72,7 +138,7 @@ finalise <- function(handle) {
                             model_config = handle$model_config,
                             submission_script_id = handle$submission_script,
                             inputs = unname(handle$inputs),
-                            outputs = outputs)
+                            outputs = as.list(handle$outputs$component_id))
 
   usethis::ui_done(paste("Record", usethis::ui_value("code run"),
                          "in local registry"))
