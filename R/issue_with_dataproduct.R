7dd720dab7c81ff47cc51b40055e4010350a5cec
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-05-26 14:06:07 +0100

    attach issue with data product

--- R/issue_with_dataproduct.R
+++ R/issue_with_dataproduct.R
@@ -1,15 +1,46 @@
-#' #' issue_with_dataproduct
-#' #'
-#' #' @param handle
-#' #' @param data_product
-#' #' @param issue
-#' #' @param severity
-#' #'
-#' #' @return
-#' #'
-#' issue_with_dataproduct <- function(handle,
-#'                                    data_product,
-#'                                    issue,
-#'                                    severity) {
+#' issue_with_dataproduct
 #'
-#' }
+#' Function to attach an issue to data product in handle. To be written to the
+#' data registry with `finalise()`.
+#'
+#' An issue can be attached to a data product referenced by an `id` (returned
+#' from `create_array()`) or
+#'
+#' @param data_product_id \code{numeric} value returned from
+#' \code{create_array()}, referencing an input / output in the handle
+#' @param handle
+#' @param data_product
+#' @param version
+#' @param issue
+#' @param severity
+#'
+#' @return
+#'
+issue_with_dataproduct <- function(data_product_id,
+                                   handle,
+                                   data_product,
+                                   version,
+                                   issue,
+                                   severity) {
+
+  if (!missing(data_product_id)) {
+
+    index_inputs <- which(handle$inputs$index %in% data_product_id)
+    if (length(index_inputs) != 0)
+      tmp <- handle$inputs[index_inputs,]
+
+    index_outputs <- which(handle$outputs$index %in% data_product_id)
+    if (length(index_outputs) != 0)
+      tmp <- handle$outputs[index_outputs,]
+
+    data_product <- tmp$data_product
+    version <- tmp$version
+  }
+
+  handle$raise_issue_dataproduct(data_product = data_product,
+                                 version = version,
+                                 issue = issue,
+                                 severity = severity)
+
+  usethis::ui_done("Noted issue in handle")
+}
