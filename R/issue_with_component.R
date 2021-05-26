3d150d6fc38a4a3e3587442b0779072a0c66cd46
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-05-26 14:07:40 +0100

    get metadata from handle

--- R/issue_with_component.R
+++ R/issue_with_component.R
@@ -5,6 +5,7 @@
 #' @param component component
 #' @param data_product data_product
 #' @param version version
+#' @param namespace namespace
 #' @param issue issue
 #' @param severity severity
 #'
@@ -15,24 +16,43 @@ issue_with_component <- function(component_id,
                                  component,
                                  data_product,
                                  version,
+                                 namespace,
                                  issue,
                                  severity) {
 
-  if (component_id %in% handle$inputs$index) {
-    index <- which(handle$inputs$index %in% component_id)
-    tmp <- handle$inputs[index,]
+  if (missing(component_id)) {
+    component_id <- NA
+
+  } else {
+    index_inputs <- which(handle$inputs$index %in% component_id)
+    index_outputs <- which(handle$outputs$index %in% component_id)
+
+    if (length(index_inputs) != 0) {
+      tmp <- handle$inputs[index_inputs,]
+      namespace <- handle$yaml$run_metadata$default_input_namespace
+
+    } else if (length(index_outputs) != 0) {
+      tmp <- handle$outputs[index_outputs,]
+      namespace <- handle$yaml$run_metadata$default_output_namespace
+
+    } else {
+      usethis::ui_oops(paste("Issue not attached: data must be referenced in the",
+                             usethis::ui_value("config.yaml"), "file"))
+      return(invisible(NULL))
+    }
+
+    component <- tmp$component
+    data_product <- tmp$data_product
+    version <- tmp$version
   }
 
-  if (component_id %in% handle$outputs$index) {
-    index <- which(handle$outputs$index %in% component_id)
-    tmp <- handle$outputs[index,]
-  }
-
-  handle$raise_issue_component(component = tmp$component,
-                               data_product = tmp$data_product,
-                               version = tmp$version,
+  handle$raise_issue_component(component_id = component_id,
+                               component = component,
+                               data_product = data_product,
+                               version = version,
+                               namespace = namespace,
                                issue = issue,
                                severity = severity)
 
-  usethis::ui_done("Noted issue in handle")
+  usethis::ui_done("Recording issue in handle")
 }
