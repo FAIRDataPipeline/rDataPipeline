370d258a4c4185e0db9c27f676ea650e13e67263
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-07 17:48:00 +0100

    attach issue to external object

--- R/issue_with_externalobject.R
+++ R/issue_with_externalobject.R
@@ -1,15 +1,28 @@
-#' #' issue_with_externalobject
-#' #'
-#' #' @param handle
-#' #' @param external_object
-#' #' @param issue
-#' #' @param severity
-#' #'
-#' #' @return
-#' #'
-#' issue_with_externalobject <- function(handle,
-#'                                       external_object,
-#'                                       issue,
-#'                                       severity) {
+#' issue_with_externalobject
 #'
-#' }
+#' @param handle \code{fdp} object
+#' @param external_object a \code{string} specifying the name of the external
+#' object
+#' @param version a \code{string} specifying the version of the external object
+#' @param issue a \code{string} describing the issue
+#' @param severity a \code{numeric} value specifying the severity of the issue
+#'
+#' @return
+#'
+issue_with_externalobject <- function(handle,
+                                      external_object,
+                                      version,
+                                      issue,
+                                      severity) {
+
+  handle$raise_issue(index = NA,
+                     component = NA,
+                     data_product = NA,
+                     external_object = external_object,
+                     version = version,
+                     namespace = NA,
+                     issue = issue,
+                     severity = severity)
+
+  usethis::ui_done("Noted issue in handle")
+}
