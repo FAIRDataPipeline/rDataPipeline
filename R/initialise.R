2c40c987e06c319782d188194ec99ff61f67d574
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-15 10:47:11 +0100

    tidy up

--- R/initialise.R
+++ R/initialise.R
@@ -28,49 +28,49 @@ initialise <- function() {
 
   run_server()
 
-  # Get working config.yaml object uri --------------------------------------
+  # Get working config.yaml object url --------------------------------------
 
   config_path <- file.path(directory, "config.yaml")
 
-  config_location_uri <- get_entry("storage_location",
+  config_location_url <- get_entry("storage_location",
                                    list(path = config_path))
-  assertthat::assert_that(length(config_location_uri) == 1)
-  config_location_id <- extract_id(config_location_uri[[1]]$url)
+  assertthat::assert_that(length(config_location_url) == 1)
+  config_location_id <- extract_id(config_location_url[[1]]$url)
 
-  config_object_uri <- get_entry("object",
+  config_object_url <- get_entry("object",
                                  list(storage_location = config_location_id))
-  assertthat::assert_that(length(config_object_uri) == 1)
-  config_object_uri <- config_object_uri[[1]]$url
+  assertthat::assert_that(length(config_object_url) == 1)
+  config_object_url <- config_object_url[[1]]$url
 
   cli::cli_alert_info(
     "Reading {.file {config_file}} metadata from local registry")
 
-  # Get script object uri ---------------------------------------------------
+  # Get script object url ---------------------------------------------------
 
   script_path <- file.path(directory, "script.sh")
 
-  script_location_uri <- get_entry("storage_location",
-                                  list(path = script_path))
-  assertthat::assert_that(length(script_location_uri) == 1)
-  script_location_id <- extract_id(script_location_uri[[1]]$url)
+  script_location_url <- get_entry("storage_location",
+                                   list(path = script_path))
+  assertthat::assert_that(length(script_location_url) == 1)
+  script_location_id <- extract_id(script_location_url[[1]]$url)
 
-  script_object_uri <- get_entry("object",
-                                list(storage_location = script_location_id))
-  assertthat::assert_that(length(script_object_uri) == 1)
-  script_object_uri <- script_object_uri[[1]]$url
+  script_object_url <- get_entry("object",
+                                 list(storage_location = script_location_id))
+  assertthat::assert_that(length(script_object_url) == 1)
+  script_object_url <- script_object_url[[1]]$url
 
   cli::cli_alert_info(
     "Reading {.file {script_file}} metadata from local registry")
 
   # record the code run in the data registry --------------------------------
 
-  coderun_uri <- new_coderun(run_date = Sys.time(),
-                            description = yaml$run_metadata$description,
-                            # code_repo_id = "",
-                            model_config_uri = config_object_uri,
-                            submission_script_uri = script_object_uri,
-                            inputs = list(),
-                            outputs = list())
+  coderun_url <- new_code_run(run_date = Sys.time(),
+                              description = yaml$run_metadata$description,
+                              # code_repo_id = "",
+                              model_config_url = config_object_url,
+                              submission_script_url = script_object_url,
+                              inputs_urls = list(),
+                              outputs_urls = list())
 
   field <- "code_run"
   cli::cli_alert_success("Writing new {.field {field}} to local registry")
@@ -79,7 +79,7 @@ initialise <- function() {
 
   # Write to handle
   fdp$new(yaml = yaml,
-          model_config = config_object_uri,
-          submission_script = script_object_uri,
-          code_run = coderun_uri)
+          model_config = config_object_url,
+          submission_script = script_object_url,
+          code_run = coderun_url)
 }
