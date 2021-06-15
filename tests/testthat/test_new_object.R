e065de3e626387b9582746e15ec4adbc52f6339f
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-15 11:48:56 +0100

    update tests

--- tests/testthat/test_new_object.R
+++ tests/testthat/test_new_object.R
@@ -1,55 +1,39 @@
 context("Testing new_object()")
 
-sleep_time <- 0.5
-
-test_user <- "22"
-
-test_identifier <- sample(1:1000000, 1, replace=TRUE)
-
-datetime <- format(Sys.time(), "%d%m%y%H%M%S")
-
-UID <- paste0("object ", datetime, test_identifier)
-abbreviation <- paste0("obj ", datetime, test_identifier)
-UID_ <- paste0("external_object_", datetime, test_identifier)
-path <- paste0(UID_, ".h5")
-path_uri <- paste0("https://", path, ".com")
+UID <- paste0("test_new_object_", openssl::sha1(x = as.character(Sys.time())))
+path <- paste0(UID, ".h5")
+path_url <- paste0("https://", path, ".com")
 hash <- paste0(Sys.time(), "%d%m%y%H%M%S")
 
 run_server()
 
-storage_root_id <- get_entry("storage_root", list(updated_by = test_user))[[1]]$url
+storage_root_url <- post_data("storage_root",
+                              list(name = UID,
+                                   root = path_url))
 
+store_url <- post_data("storage_location",
+                       list(path = path,
+                            hash = hash,
+                            storage_root = storage_root_url))
 
-if(is.null(storage_root_id)){
-  storage_root_id <- post_data("storage_root",
-                               list(name = UID,
-                                    root = path_uri))
-}
-
-store_id <- post_data("storage_location",
-                      list(path = path,
-                           hash = hash,
-                           storage_root = storage_root_id))
-
-
-test_that("new_object creates a new object", {
-  expect_true(is.character(new_object("",
-                                      description = UID)))
+test_that("new entry in object returns API URL", {
+  expect_true(grepl("object", new_object("",
+                                         description = UID)))
 })
 
-test_that("new_object creates a new object", {
-  expect_true(is.character(new_object(store_id,
-                                      description = UID)))
+test_that("new entry in object returns API URL", {
+  expect_true(grepl("object", new_object(store_url,
+                                         description = UID)))
 })
 
-test_that("new_object creates a new object", {
-  expect_true(is.character(new_object("",
-                                      description = "")))
+test_that("new entry in object returns API URL", {
+  expect_true(grepl("object", new_object("",
+                                         description = "")))
 })
 
-test_that("new_object returns URI when the object exists", {
-  expect_true(is.character(new_object(store_id,
-                                      description = UID)))
+test_that("existing entry in object returns API URL", {
+  expect_true(grepl("object", new_object(store_url,
+                                         description = UID)))
 })
 
 stop_server()
