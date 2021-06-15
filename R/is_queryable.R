805818f3fb55bfff6ec1101086a504570d092675
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-15 11:01:52 +0100

    add checks

--- R/is_queryable.R
+++ R/is_queryable.R
@@ -3,21 +3,33 @@
 #' Check whether fields are queryable
 #'
 #' @param table a \code{string} specifying the name of the table
-#' @param query_parameter a \code{string} or \code{vector} of field names
+#' @param query a \code{string} or \code{vector} of field names
 #'
 #' @return Returns \code{TRUE} if the entry is queryable and \code{FALSE} if it
 #' isn't
 #' @export
 #' @keywords internal
 #'
-is_queryable <- function(table, query_parameter) {
-  if(table == "users" | table == "groups")
-    return(FALSE) # only queryable with token
+is_queryable <- function(table, query) {
 
-  # if(!check_table_exists(table)) {
-  #   message("table doesn't exist")
-  #   return(FALSE)
-  # }
+  valid_fields <-  all(query %in% get_table_queryable(table))
 
-  query_parameter %in% get_table_queryable(table)
+  if (table == "users" | table == "groups") {
+    # Only queryable with token
+    return(FALSE)
+
+  } else if (!check_table_exists(table)) {
+    message("Table doesn't exist")
+    return(FALSE)
+
+  } else if (any(is.character(query) & query == "")) {
+    # An empty string is not a valid query
+    return(FALSE)
+
+  } else if (!valid_fields) {
+    return(FALSE)
+
+  } else {
+    return(TRUE)
+  }
 }
