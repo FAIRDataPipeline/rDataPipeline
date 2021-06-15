f70a26b6c0b50c4238cf4aa0bf2621f5a751f5c8
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-15 11:01:23 +0100

    tidy up and add server script interaction

--- R/check_exists.R
+++ R/check_exists.R
@@ -23,24 +23,28 @@
 check_exists <- function(table,
                          query) {
 
-    output <- httr::GET(paste0("http://localhost:8000/api/", table, ""),
-                        query = query)
+  run_server()
 
-    if(any(names(output) == "status_code")){
-      if(output$status_code == 404)
-        stop("an error occured: does the table exist?")
-      else if(output$status_code == 200){
+  output <- httr::GET(paste0("http://localhost:8000/api/", table, ""),
+                      query = query)
+
+  stop_server()
+
+  if(any(names(output) == "status_code")){
+    if(output$status_code == 404)
+      stop("Table does not exist")
+    else if(output$status_code == 200){
       output <- output %>% httr::content(as = "text", encoding = "UTF-8") %>%
-      jsonlite::fromJSON(simplifyVector = FALSE)
+        jsonlite::fromJSON(simplifyVector = FALSE)
 
       if(any(names(output) == "detail")){
-       if(grepl("Invalid", output$detail))
-         stop(output["detail"])}
+        if(grepl("Invalid", output$detail))
+          stop(output["detail"])}
 
       if(any(names(output) == "count"))
         return(ifelse(output$count > 0, TRUE, FALSE))}
-      else
-        stop(paste("error: status:", output$status_code, "returned"))}
     else
-      stop("something went wrong")
+      stop(paste("error: status:", output$status_code, "returned"))}
+  else
+    stop("something went wrong")
 }
