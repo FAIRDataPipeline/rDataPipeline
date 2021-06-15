4be6e3f98fb766c882505b51e3b08b074c006ce7
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-15 10:54:04 +0100

    tidy up

--- R/new_data_product.R
+++ R/new_data_product.R
@@ -5,9 +5,9 @@
 #' @param name a \code{string} specifying the name of the \code{data_product}
 #' @param version a \code{string} specifying the version identifier of the
 #' \code{data_product} (must conform to semantic versioning syntax)
-#' @param object_uri a \code{string} specifying the URI of the entry in the
+#' @param object_url a \code{string} specifying the URL of the entry in the
 #' \code{object} table
-#' @param namespace_uri a \code{string} specifying the URI of the entry in the
+#' @param namespace_url a \code{string} specifying the URL of the entry in the
 #' \code{namespace} table
 #'
 #' @family new functions
@@ -16,12 +16,12 @@
 #'
 new_data_product <- function(name,
                              version,
-                             object_uri,
-                             namespace_uri) {
+                             object_url,
+                             namespace_url) {
 
   post_data(table = "data_product",
             data =  list(name = name,
                          version = version,
-                         object = object_uri,
-                         namespace = namespace_uri))
+                         object = object_url,
+                         namespace = namespace_url))
 }
