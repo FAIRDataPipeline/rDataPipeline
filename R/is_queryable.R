787c2159d18d3a3ed71388fe13561684ea9f3eaa
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-16 22:08:12 +0100

    add checks

--- R/is_queryable.R
+++ R/is_queryable.R
@@ -3,7 +3,7 @@
 #' Check whether fields are queryable
 #'
 #' @param table a \code{string} specifying the name of the table
-#' @param query a \code{string} or \code{vector} of field names
+#' @param query a \code{list} containing the query
 #'
 #' @return Returns \code{TRUE} if the entry is queryable and \code{FALSE} if it
 #' isn't
@@ -12,22 +12,33 @@
 #'
 is_queryable <- function(table, query) {
 
-  valid_fields <-  all(query %in% get_table_queryable(table))
+  # Check whether field names are valid
+
+  fields <- names(query)
+  valid_fields <- all(fields %in% get_table_queryable(table))
+
+  # Check whether the class of each field in the query matches what is expected
+
+  valid_query <- !(any(check_fields(table, query) == FALSE))
+
+  # Output
 
   if (table == "users" | table == "groups") {
     # Only queryable with token
-    return(FALSE)
+    usethis::ui_stop("Unable to query {ui_field(table)}")
 
   } else if (!check_table_exists(table)) {
-    message("Table doesn't exist")
-    return(FALSE)
+    usethis::ui_stop("{ui_field(table)} does not exist")
 
-  } else if (any(is.character(query) & query == "")) {
-    # An empty string is not a valid query
-    return(FALSE)
+  } else if (any(is.character(fields) & fields == "")) {
+    # An empty string is not a valid field
+    usethis::ui_stop("{ui_field(fields)} contains invalid fields")
 
   } else if (!valid_fields) {
-    return(FALSE)
+    usethis::ui_stop("{ui_field(fields)} contains invalid fields")
+
+  } else if (!valid_query) {
+    usethis::ui_stop("query contains fields of incorrect class")
 
   } else {
     return(TRUE)
