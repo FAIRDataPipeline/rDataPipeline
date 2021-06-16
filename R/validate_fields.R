f05e46ca1530627af65c03ba67a672fde943a2a2
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-16 23:44:50 +0100

    reintegrate field validation and update tests

---
+++ R/validate_fields.R
@@ -0,0 +1,202 @@
+#' Validate fields
+#'
+#' Function to validate fields in post data
+#'
+#' @param table table name as character
+#' @param data data as a named list
+#'
+#' @return Returns
+#' @export
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' \donttest{
+#' validate_fields("author", list(object = 123, family_name = "Smith",
+#'                                personal_name = "S"), key)
+#' }}
+#'
+validate_fields <- function(table, data){
+
+  table.fields <- get_fields(table)
+
+  table.required <- table.fields %>%
+    filter(.data$required)
+
+  # Check object
+  if(is.null(data) & nrow(table.required) == 0) {
+    warning("Data is Null")
+  } else if(!is.list(data)) {
+    stop("data must be a list")
+  }
+
+  # Ensure fields are writable
+  table.writable <- table.fields %>%
+    filter(!.data$read_only)
+  fields.required <- character(0)
+  data.field_names <- names(data)
+
+  for(field_i in seq_along(table.required$field)){
+    type <- table.required[field_i,]$data_type
+    name <- table.required[field_i,]$field
+    # If a field is plural it can sometimes be not required or empty even
+    # though they're listed as required, e.g component_issues and object_issues
+    # on issues or input_of and output_of on object component
+    doesnt_end_in_of <- grepl(".*?s_of$", name)
+    doesnt_end_in_s <- grepl(".*?s$", name)
+    if(type == "field" & (doesnt_end_in_s | doesnt_end_in_of)) {
+      #
+    } else
+      fields.required <- c(fields.required, name)
+  }
+
+  if(!all(data.field_names %in% table.writable$field))
+    stop(paste0("Invalid field names valid names are: ",
+                paste0(table.writable$field, collapse = " ")))
+
+  # Ensure all required fields are present
+  if(!all(fields.required %in% data.field_names))
+    stop(paste0("required fields are missing required field names are: ",
+                paste0(table.required, collapse = " ")))
+
+  # Loop through data list
+  for(i in seq_along(data)) {
+
+    # Extract relevent variables from data and writable fields
+    value <- data[[i]]
+    name <- names(data[i])
+    type <- table.writable[table.writable$field == name,]$data_type
+    max_length <- table.writable[table.writable$field == name,]$max_length
+    min_length <- table.writable[table.writable$field == name,]$min_length
+    choice_values <- table.writable[table.writable$field == name,]$choice_values
+    choice_names <- table.writable[table.writable$field == name,]$choice_names
+
+    # Allow empty string (cast to character as a posix date will cause an error)
+    # or null if an optional field
+    name_missing <- !name %in% table.required$field
+    char_value <- isTRUE(is.character(value) & as.character(value) =="")
+    null_value <- isTRUE(is.null(value) | length(value) == 0)
+
+    # If name is missing ------------------------------------------------------
+    if (name_missing & (char_value | null_value)){
+      # Do nothing. Empty string in an optional field will be set to NULL in
+      # the database
+
+      # If the type is a field, expect api urls  ---------------------------------
+    } else if (type == "field") {
+      # Check if the field is plural
+      if (grepl(".*?s$", name) |  grepl(".*?s_of$", name)) {
+        # if so expect a list
+        if (is.list(value) & length(value) == 0) {
+          # allow empty list
+        } else if (!is.list(value)) {
+          stop(paste0(name, " must be a list"))
+        } else {
+          # if a list expect a list of api objects
+          for (i in seq_along(value)) {
+            if (!grepl("http://localhost:8000/api/.+/.+", value[[i]])) {
+              stop(paste0(name, " Must be an api url"))
+            } else if(!get_entity(value[[i]])$url == value[[i]]){
+              stop(paste0(name, " must be in the data registry"))
+            }
+          }
+        }
+
+        # If the field isn't plural expect a character of an api url
+      } else if(is.character(value)){
+        if (!grepl("http://localhost:8000/api/.+/.+", value)) {
+          stop(paste0(name, " Must be an api url"))
+        } else if(!get_entity(value)$url == value) {
+          stop(paste0(name, " must be in the data registry"))
+        }
+      } else {
+        stop(paste0(name, " field must be either a character or a list"))
+      }
+
+      # If field is a string ----------------------------------------------------
+
+    } else if(type == "string") {
+      # API will accept anything castable to a string so check it can be cast
+      # to a string (character)
+      if (suppressWarnings(is.na(as.character(value))))
+        stop(paste0(name, " must be of type ", type))
+
+      # If there is a maximum length, check the character does not exceed it
+      if(!is.na(max_length)){
+        if(!is.na(max_length) & nchar(value) > max_length)
+          stop(paste0("Maximum Length of string exceeded Max Length: ", max_length))
+      }
+
+      if(name == "version"){
+        if(!grepl("^.*[.].*[.].*$", value))
+          stop(paste0(name, " must be of syntatic type version e.g. 0.1.0"))
+      }
+
+      # If the field is a date --------------------------------------------------
+
+    } else if (type == "datetime") {
+      # Check the date can be cast to a posix
+      if(is.na.POSIXlt(value)){
+        stop(paste0(name, " must be of type ", type))
+      }
+
+      # If the field is a URL ---------------------------------------------------
+
+    } else if(type == "url"){
+      # Expect a character
+      if(!is.character(value)){
+        stop(paste0(name, " must be of type ", type))
+      }
+      # Expect there to be a :// to allow for https:// http:// ftp:// etc.
+      else if(!grepl("://", value)){
+        stop(paste0("Expecting a URL got: ", value))
+      }
+
+      # If the field is a choice ------------------------------------------------
+
+    } else if (type == "choice"){
+      # Expect either a string or an integer
+      if(!is.character(value) & !is.integer(value) & !is.numeric(value)){
+        stop(paste0(name, " must be of type ", type))
+      }
+      # Expect a valid choice
+      else{
+        if(!grepl(as.character(value), choice_values)){
+          stop(paste0("Must be a valid choice, valid choices are: ",
+                      choice_values, " Which correspond to ", choice_names))
+        }
+      }
+
+      # If the field is a integer -----------------------------------------------
+
+    } else if(type == "integer") {
+      # Accept a string or integer
+      if(!is.character(value) & !is.integer(value) & !is.numeric(value)){
+        stop(paste0(name, " must be of type ", type))
+      } else{
+        # If a character make sure it's a valid integer
+        if(suppressWarnings(is.na(as.integer(value))))
+          stop(paste0(name, " must be of type ", type))
+
+        if(!is.na(min_length) & as.integer(value) < min_length)
+          stop(paste0(name, " must be greater than ", min_length))
+
+        if(!is.na(max_length) & as.integer(value) > max_length)
+          stop(paste0(name, " must be less than ", max_length))
+      }
+
+      # If the field is a boolean -----------------------------------------------
+
+    } else if(type == "boolean"){
+      # Accept character
+      if (!is.character(value) & !is.logical(value)) {
+        stop(paste0(name, " must be of type ", type))
+      } else if(is.character(value)) {
+        if(!tolower(value) == "true" & !tolower(value) == "false"){
+          stop(paste0(name, " must be of type ", type))
+        }
+      }
+    }
+  }
+  data
+}
--- tests/testthat/test_validate_fields.R
+++ tests/testthat/test_validate_fields.R
@@ -1,47 +1,38 @@
 context("Testing validate_fields()")
 
-sleep_time <- 0.5
-
-key <- Sys.getenv("SCRC_API_TOKEN")
-
-test_user <- "22"
+run_server()
 
 #get all tables
 tables <- get_tables()
 unknown_table <- "unknown"
 
-test_identifier <- sample(1:1000000, 1, replace=TRUE)
+UID <- paste0("test_validate_fields_ ",
+              openssl::sha1(x = as.character(Sys.time())))
 
-datetime <- format(Sys.time(), "%d%m%y%H%M%S")
+object_id <- post_data("object",
+                       list(description = UID))
 
-UID <- paste0("object ", datetime, test_identifier)
-
-object_id <- get_entry("object", list(updated_by = test_user))[[1]]$url
-
-if(is.null(object_id)){
-  object_id <- post_data("object",
-                         list(description = UID),
-                         key)
-}
 
 test_that("incorrect tables produce and error", {
-  expect_error(validate_fields(unknown_table, data = list(), key))
-  expect_error(validate_fields(NULL, data = list(), key))
+  expect_error(validate_fields(unknown_table, data = list()))
+  expect_error(validate_fields(NULL, data = list()))
 })
 
 test_that("validate_fields works with all tables",{
-  for(i in seq_along(tables)){
+  for (i in seq_along(tables)) {
+    print(i)
     table <- tables[i]
-    if(table == "users" | table == "groups") {
-      expect_error(validate_fields(tables[i], data = list("username = test"), key))
-    } else{
-      Sys.sleep(sleep_time)
-      table.writable <- get_table_writable(table, key)
-      Sys.sleep(sleep_time)
-      table.required <- get_table_required(table, key)
-      if(nrow(table.required)  > 1){
-        expect_error(validate_fields(table, data = list(), key))
-      }
+
+    if (table == "users" | table == "groups") {
+      expect_error(validate_fields(tables[i], data = list("username = test")))
+
+    } else {
+      table.writable <- get_table_writable(table)
+      table.required <- get_table_required(table)
+
+      if(nrow(table.required)  > 1)
+        expect_error(validate_fields(table, data = list()))
+
       data_correct <- list()
       data_correct_2 <- list()
       data_correct_3 <- list()
@@ -50,11 +41,12 @@ test_that("validate_fields works with all tables",{
       data_incorrect_2 <- list()
       data_incorrect_3 <- list()
       data_incorrect_4 <- list()
-      for(ii in seq_along(table.writable$field)){
+
+      for(ii in seq_along(table.writable$field)) {
         field = table.writable$field[ii]
         data_type = table.writable$data_type[ii]
 
-        max_value = table.writable$max_value[ii]
+        max_length = table.writable$max_length[ii]
 
         data_incorrect[[field]] <- dplyr::case_when(
           data_type == "field" ~ "https://not.data.uk",
@@ -73,79 +65,66 @@ test_that("validate_fields works with all tables",{
           data_type == "url" ~ "https://test.com",
           data_type == "choice" ~ "0",
           data_type == "datetime" ~ as.character(Sys.time()),
-          data_type == "boolean" ~ "TRUE"
-        )
-        if(data_type == "field" & (grepl(".*?s$", field) |
-                                   grepl(".*?s_of$", field))){
+          data_type == "boolean" ~ "TRUE")
+
+        if (data_type == "field" & (grepl(".*?s$", field) |
+                                    grepl(".*?s_of$", field))) {
           data_correct[[field]] <- list(object_id)
         }
 
-        if(!field %in% table.required$field)
-        {
+        if (!field %in% table.required$field) {
           data_correct_2[[field]] <- ""
           data_correct_3[[field]] <- NULL
           data_incorrect_4[[field]] <- "test"
-        }
-        else{
+        } else {
           data_correct_2[[field]] <- data_correct[[field]]
           data_correct_3[[field]] <- data_correct[[field]]
         }
 
-        if(data_type == "integer")
-        {
+        if (data_type == "integer") {
           data_correct_4[[field]] <- 1
           data_incorrect_2[[field]] <- Sys.time()
-          if(!is.na(max_value))
-          {
-            data_incorrect_3[[field]] <- max_value + 1
+          if(!is.na(max_length)) {
+            data_incorrect_3[[field]] <- max_length + 1
           }
-        }
-        else if(data_type == "choice"){
+        } else if(data_type == "choice") {
           data_correct_4[[field]] <- 0
           data_incorrect_2[[field]] <- Sys.time()
           data_incorrect_3[[field]] <- 1000000000000000000000
-        }
-        else if(data_type == "datetime"){
+        } else if(data_type == "datetime") {
           data_correct_4[[field]] <- Sys.time()
           data_incorrect_2[[field]] <- "011110002"
           data_incorrect_3[[field]] <- 1000000000000000000000
-        }
-        else if(data_type == "boolean"){
+        } else if(data_type == "boolean"){
           data_correct_4[[field]] <- TRUE
           data_incorrect_2[[field]] <- Sys.time()
           data_incorrect_3[[field]] <- 1000000000000000000000
-        }
-        else
-        {
+        } else {
           data_correct_4[[field]] <- data_correct[[field]]
           data_incorrect_2[[field]] <- data_incorrect[[field]]
           data_incorrect_3[[field]] <- data_incorrect[[field]]
         }
-        if(data_type == "string" & !is.na(max_value))
-        {
-          data_incorrect[[field]] <- paste(rep("t", max_value + 1), collapse = "")
-          data_incorrect_2[[field]] <- paste(rep("t", max_value + 1), collapse = "")
-          data_incorrect_3[[field]] <- paste(rep("1", max_value + 1), collapse = "")
-          data_incorrect_4[[field]] <- paste(rep("1", max_value + 1), collapse = "")
+
+        if (data_type == "string" & !is.na(max_length)) {
+          data_incorrect[[field]] <- paste(rep("t", max_length + 1), collapse = "")
+          data_incorrect_2[[field]] <- paste(rep("t", max_length + 1), collapse = "")
+          data_incorrect_3[[field]] <- paste(rep("1", max_length + 1), collapse = "")
+          data_incorrect_4[[field]] <- paste(rep("1", max_length + 1), collapse = "")
         }
 
       }
-      expect_true(is.list(validate_fields(table, data_correct, key)))
-      Sys.sleep(sleep_time)
-      expect_true(is.list(validate_fields(table, data_correct_2, key)))
-      Sys.sleep(sleep_time)
-      expect_true(is.list(validate_fields(table, data_correct_3, key)))
-      Sys.sleep(sleep_time)
-      expect_true(is.list(validate_fields(table, data_correct_4, key)))
-      Sys.sleep(sleep_time)
+      expect_true(is.list(validate_fields(table, data_correct)))
+      expect_true(is.list(validate_fields(table, data_correct_2)))
+      expect_true(is.list(validate_fields(table, data_correct_3)))
+      expect_true(is.list(validate_fields(table, data_correct_4)))
 
-      expect_error(validate_fields(table, data_incorrect, key))
-      Sys.sleep(sleep_time)
-      expect_error(validate_fields(table, data_incorrect_2, key))
-      Sys.sleep(sleep_time)
-      expect_error(validate_fields(table, data_incorrect_3, key))
-      Sys.sleep(sleep_time)
-      expect_error(validate_fields(table, data_incorrect_4, key))
+      expect_error(validate_fields(table, data_incorrect))
+      expect_error(validate_fields(table, data_incorrect_2))
+      expect_error(validate_fields(table, data_incorrect_3))
+      expect_error(validate_fields(table, data_incorrect_4))
 
-    }}
+    }
+  }
 })
+
+stop_server()
