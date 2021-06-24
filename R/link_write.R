0b57adb9aee4b0103461d7cf51426e31e6c2495d
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-24 22:39:08 +0100

    initial commit

---
+++ R/link_write.R
@@ -0,0 +1,47 @@
+#' link_write
+#'
+#' @param handle an object of class \code{fdp, R6} containing metadata required
+#' by the Data Pipeline API
+#' @param name a \code{string} representing an external object in the
+#' config.yaml file
+#'
+#' @export
+#'
+link_write <- function(handle, name) {
+
+  # Get object metadata from working config.yaml
+  write <- handle$yaml$write
+  index <- lapply(write, function(x)
+    name == x$data_product) %>% unlist() %>% which()
+
+  if (length(index) == 0)
+    usethis::ui_stop("{name} not present in config.yaml")
+
+  this_write <- write[[index]]
+
+  if ("use" %in% names(this_write)) {
+    alias <- this_write$use
+  } else {
+    alias <- list()
+  }
+
+  datastore <- handle$yaml$run_metadata$write_data_store
+
+  if ("namespace" %in% names(alias)) {
+    namespace <- alias$namespace
+  } else {
+    namespace <- handle$yaml$run_metadata$default_output_namespace
+  }
+
+  if ("data_product" %in% names(alias)) {
+    data_product <- alias$data_product
+  } else {
+    data_product <- this_write$data_product
+  }
+
+  file_type <- this_write$file_type
+  filename <- paste0(format(Sys.time(), "%Y%m%d-%H%M%S"), ".", file_type)
+
+  path <- file.path(paste0(datastore, namespace), data_product, filename)
+  invisible(path)
+}
