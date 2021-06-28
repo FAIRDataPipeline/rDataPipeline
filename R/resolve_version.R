c6710983d90d6b920a38f3fae6f4fe31bc3eee23
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-06-28 13:15:32 +0100

    implement increment version in write

--- R/resolve_version.R
+++ R/resolve_version.R
@@ -1,16 +1,33 @@
 #' resolve_version
 #' @param version version number
+#' @param data_product
+#' @param namespace_id
 #'
-resolve_version <- function(version) {
-  # Take care of variables
+resolve_version <- function(version, data_product, namespace_id) {
+
+  # Increment patch ---------------------------------------------------------
+
   if (grepl("\\$\\{\\{PATCH\\}\\}", version)) {
-    stop("Not written")
+    max_version <- get_max_version(data_product, namespace_id)
+    patch <- max_version[[1]]$patch
+    max_version[[1]]$patch <- as.integer(patch + 1)
+    write_version <- as.character(max_version)
+
+    # Increment minor ---------------------------------------------------------
 
   } else if (grepl("\\$\\{\\{MINOR\\}\\}", version)) {
-    stop("Not written")
+    max_version <- get_max_version(data_product, namespace_id)
+    minor <- max_version[[1]]$minor
+    max_version[[1]]$minor <- as.integer(minor + 1)
+    write_version <- as.character(max_version)
+
+    # Increment major ---------------------------------------------------------
 
   } else if (grepl("\\$\\{\\{MAJOR\\}\\}", version)) {
-    stop("Not written")
+    max_version <- get_max_version(data_product, namespace_id)
+    major <- max_version[[1]]$major
+    max_version[[1]]$major <- as.integer(major + 1)
+    write_version <- as.character(max_version)
 
   } else if (grepl("\\$\\{\\{DATETIME\\}\\}", version)) {
     datetime <- gsub("-", "", Sys.Date())
