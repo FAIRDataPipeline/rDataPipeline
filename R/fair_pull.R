6408d96da8c78e51656f1d0f26a3a43399267f35
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-07-06 15:25:56 +0100

    check for existing files / data products

--- R/fair_pull.R
+++ R/fair_pull.R
@@ -6,6 +6,8 @@
 #'
 fair_pull <- function(path = "config.yaml") {
 
+  run_server()
+
   # Read config.yaml --------------------------------------------------------
 
   if (file.exists(path)) {
@@ -13,9 +15,6 @@ fair_pull <- function(path = "config.yaml") {
     usethis::ui_info(paste("Reading", usethis::ui_value("config.yaml")))
   } else
     usethis::ui_stop(paste(usethis::ui_value(path), "does not exist"))
-
-  datastore <- yaml$run_metadata$write_data_store
-  namespace <- yaml$run_metadata$default_output_namespace
 
   # Download any data required by `read:` from the remote data store and record
   # metadata in the data registry -------------------------------------------
@@ -41,58 +40,15 @@ fair_pull <- function(path = "config.yaml") {
       register <- list(register)
 
     # Find entry to register
-    types <- c("external_object", "data_product")
+    types <- "external_object"
     entries <- lapply(register, function(x)
       lapply(types, function(y) x[[y]]) %>% unlist())
 
     # Download raw data to data store and register in data registry
     for (i in seq_along(register)) {
-
-      register_this <- register[[i]]
-
-      # Local data store location
-      local_path <- file.path(paste0(datastore, namespace),
-                              register_this$external_object)
-      tmp_filename <- paste(openssl::sha1(as.character(Sys.time())),
-                            register_this$file_type, sep = ".")
-
-      # Download data
-      if (grepl("SELECT", register_this$path) &
-          grepl("WHERE", register_this$path)) {
-        download_from_database(source_root = register_this$root,
-                               source_path = register_this$path,
-                               path = local_path,
-                               filename = tmp_filename)
-      } else {
-        download_from_url(source_root = register_this$root,
-                          source_path = register_this$path,
-                          path = local_path,
-                          filename = tmp_filename)
-      }
-
-      # Rename data file
-      hash <- get_file_hash(file.path(local_path, tmp_filename))
-      new_filename <- paste(hash, register_this$file_type, sep = ".")
-      file.rename(file.path(local_path, tmp_filename),
-                  file.path(local_path, new_filename))
-
-      usethis::ui_done(
-        paste("Downloading", usethis::ui_value(register_this$external_object),
-              "to local data store"))
-
-      # Update local data registry
-
-      if ("external_object" %in% names(register_this)) {
-
-        externalobject_url <- register_external_object(
-          register_this = register_this,
-          datastore = datastore,
-          namespace = namespace,
-          filename = new_filename)
-      } else {
-        stop("Code not written")
-      }
-
+      register_external_object(yaml = yaml,
+                               register_this = register[[i]])
     }
   }
+
 }
