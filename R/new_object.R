d7fc34e3635325156e7c7ed702acc6c7e348b19f
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-07-14 11:54:33 +0100

    remove field

--- R/new_object.R
+++ R/new_object.R
@@ -6,8 +6,6 @@
 #' description of the \code{object}
 #' @param storage_location_url (optional) a \code{string} specifying the URL
 #' of an entry in the \code{storage_location} table
-#' @param authors_urls (optional) a \code{list} of URLs referencing
-#' \code{author}s to associate with this \code{object}
 #' @param file_type_url (optional) a \code{string} specifying the URL
 #' of an entry in the \code{file_type} table
 #' @param endpoint a \code{string} specifying the registry endpoint
@@ -18,7 +16,6 @@
 #'
 new_object <- function(description,
                        storage_location_url,
-                       authors_urls = list(),
                        file_type_url,
                        endpoint = "http://localhost:8000/api/") {
 
@@ -29,9 +26,6 @@ new_object <- function(description,
 
   if (!missing(storage_location_url))
     data$storage_location <- storage_location_url
-
-  if (!missing(authors_urls))
-    data$authors <- authors_urls
 
   if (!missing(file_type_url))
     data$file_type <- file_type_url
