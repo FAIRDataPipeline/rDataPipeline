7584c33748cb102bafbf583b19a3bca541fd8175
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-07-14 17:13:23 +0100

    fix cmd check

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,5 @@
 # library(testthat)
-# library(rFDP)
+library(rFDP)
 
 
 Sys.setenv(FDP_endpoint = "http://localhost:8000/api/")
@@ -9,9 +9,9 @@ Sys.setenv(FDP_endpoint = "http://localhost:8000/api/")
 
 endpoint <- Sys.getenv("FDP_endpoint")
 
-fair_init(family_name = "Mitchell",
-          given_name = "Sonia",
-          orcid = "https://orcid.org/0000-0003-1536-2066",
-          organisation = c("University of Glasgow",
-                           "Boydorr Centre for Population and Ecosystem Health"),
-          endpoint = endpoint)
+rFDP::fair_init(family_name = "Mitchell",
+                given_name = "Sonia",
+                orcid = "https://orcid.org/0000-0003-1536-2066",
+                organisation = c("University of Glasgow",
+                                 "Boydorr Centre for Population and Ecosystem Health"),
+                endpoint = endpoint)
