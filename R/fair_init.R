cecde703993edc2d467ff849afd14a02419a2902
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-07-14 11:53:34 +0100

    initial commit

---
+++ R/fair_init.R
@@ -0,0 +1,51 @@
+#' fair_init
+#'
+#' @param family_name a \code{string} specifying family name
+#' @param given_name a \code{string} specifying given name(s)
+#' @param orcid a \code{string} specifying an orcid id URL
+#' @param organisation a \code{string} or vector of \code{strings} specifying
+#' the name of the organisation(s)
+#' @param ror a \code{string} specifying the organisation identifer (ROR ID) URL
+#' @param endpoint a \code{string} specifying the registry endpoint
+#'
+#' @export
+#'
+fair_init <- function(family_name,
+                      given_name,
+                      orcid,
+                      organisation,
+                      ror,
+                      endpoint = "http://localhost:8000/api/") {
+
+  user_url <- get_url(table = "users",
+                      query = list(username = "admin"),
+                      endpoint = endpoint)
+
+  if (missing(orcid)) {
+    author_url <- new_author(family_name = family_name,
+                             given_name = given_name,
+                             endpoint = endpoint)
+  } else {
+    author_url <- new_author(family_name = family_name,
+                             given_name = given_name,
+                             identifier = orcid,
+                             endpoint = endpoint)
+  }
+
+  if (missing(ror)) {
+    organisation_urls <- lapply(organisation, function(x)
+      new_organisation(name = x,
+                       endpoint = endpoint))
+  } else {
+    organisation_urls <- lapply(organisation, function(x)
+      new_organisation(name = x,
+                       ror = ror,
+                       endpoint = endpoint))
+
+  }
+
+  new_user_author_org(user_url = user_url,
+                      author_url = author_url,
+                      organisations_urls = organisation_urls,
+                      endpoint = endpoint)
+}
