6ff4f7ca6bc2e73396d58d6d661568623fb5fe60
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-05 10:59:20 +0100

    remove fields

--- R/new_author.R
+++ R/new_author.R
@@ -2,22 +2,23 @@
 #'
 #' Upload information to the \code{author} table in the data registry
 #'
-#' @param family_name a \code{string} specifying the author's family name
-#' @param given_name a \code{string} specifying the author's first name
-#' and / or middle name and / or any initials
-#' @param identifier (optional) a \code{string} specifying the full URL of
-#' the \code{author}s identifier, *e.g.* ORCID iD
+#' @param name a \code{string} specifying the full name or organisation name of
+#' the \code{author}; note that at least one of name or identifier must be
+#' specified
+#' @param identifier (optional) a \code{string} specifying the full URL
+#' identifier (*e.g.* ORCiD or ROR ID) of the \code{author}
 #' @param endpoint a \code{string} specifying the registry endpoint
 #'
 #' @family new functions
 #'
-new_author <- function(family_name,
-                       given_name,
+new_author <- function(name,
                        identifier,
                        endpoint = "http://localhost:8000/api/") {
 
-  data <- list(family_name = family_name,
-               given_name = given_name)
+  data <- list()
+
+  if (!missing(name))
+    data$name <- name
 
   if (!missing(identifier))
     data$identifier <- identifier
