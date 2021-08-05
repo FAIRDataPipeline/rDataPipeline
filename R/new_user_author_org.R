61a32bccd1560745eafc3bf970d732a51e36b5ff
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-05 10:58:29 +0100

    rename function to match table, remove optional organisations field

--- R/new_user_author_org.R
+++ R/new_user_author_org.R
@@ -1,29 +1,23 @@
-#' Post entry to user_author_org table
+#' Post entry to user_author table
 #'
-#' Upload information to the \code{user_author_org} table in the data registry
+#' Upload information to the \code{user_author} table in the data registry
 #'
 #' @param user_url a \code{string} specifying the URL of an existing
 #' \code{user}
 #' @param author_url a \code{string} specifying the URL of an existing
 #' \code{author}
-#' @param organisations_urls (optional) a \code{list} of URLs specifying which
-#' \code{organisation}s to associate with this \code{user_author_org}
 #' @param endpoint a \code{string} specifying the registry endpoint
 #'
 #' @family new functions
 #'
-new_user_author_org <- function(user_url,
-                                author_url,
-                                organisations_urls = list(),
-                                endpoint = "http://localhost:8000/api/") {
+new_user_author <- function(user_url,
+                            author_url,
+                            endpoint = "http://localhost:8000/api/") {
 
   data <- list(user = user_url,
                author = author_url)
 
-  if (!missing(organisations_urls))
-    data$organisations <- organisations_urls
-
-  post_data(table = "user_author_org",
+  post_data(table = "user_author",
             data =  data,
             endpoint = endpoint)
 }
