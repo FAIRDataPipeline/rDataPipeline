c865726ee967dff5861ed78d9ff81829c889068d
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-11 10:57:22 +0100

    add globbing examples

--- tests/testthat/test_examples.R
+++ tests/testthat/test_examples.R
@@ -448,4 +448,164 @@ finalise(handle)
 
 # CodeRun with read globbing ------------------------------------------------
 
+# 1 - Write to registry
+
+uid <- as.character(random_hash())
+data_product1 <- file.path("real", "data", uid, "1")
+data_product2 <- file.path("real", "data", uid, "thing", "1")
+coderun_description <- "Register a file in the pipeline"
+dataproduct_description <- "A csv file"
+namespace1 <- "username"
+
+# User written config file
+config_file <- paste0("config_files/20a/config_", uid , ".yaml")
+create_config(path = config_file,
+              description = coderun_description,
+              input_namespace = namespace1,
+              output_namespace = namespace1)
+add_write(path = config_file,
+          data_product = data_product1,
+          description = dataproduct_description,
+          file_type = "csv")
+add_write(path = config_file,
+          data_product = data_product2,
+          description = dataproduct_description,
+          file_type = "csv")
+
+# CLI functions
+fair_pull(path = config_file)
+fair_run(path = config_file, skip = TRUE)
+
+# Initialise code run
+config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
+script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+handle <- initialise(config, script)
+
+# Write data
+path1 <- link_write(handle, data_product1)
+uid1 <- paste0(uid, "_1")
+df1 <- data.frame(a = uid1, b = uid1)
+write.csv(df1, path1)
+
+path2 <- link_write(handle, data_product2)
+uid2 <- paste0(uid, "_2")
+df2 <- data.frame(a = uid2, b = uid2)
+write.csv(df2, path2)
+
+finalise(handle)
+
+# Input globbing
+
+data_product3 <- file.path("real", "data", uid, "*")
+
+# User written config file
+config_file <- paste0("config_files/20b/config_", uid , ".yaml")
+create_config(path = config_file,
+              description = coderun_description,
+              input_namespace = namespace1,
+              output_namespace = namespace1)
+add_read(path = config_file,
+         data_product = data_product3)
+
+# CLI functions
+fair_pull(path = config_file)
+fair_run(path = config_file, skip = TRUE)
+
+# Initialise code run
+config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
+script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+handle <- initialise(config, script)
+
+path <- link_read(handle, data_product1)
+path <- link_read(handle, data_product2)
+
 # CodeRun with write globbing -----------------------------------------------
+
+# 2 - Write to registry
+
+uid <- as.character(random_hash())
+data_product1 <- file.path("real", "data", uid, "1")
+data_product2 <- file.path("real", "data", uid, "thing", "1")
+coderun_description <- "Register a file in the pipeline"
+dataproduct_description <- "A csv file"
+namespace1 <- "username"
+
+# User written config file
+config_file <- paste0("config_files/20b/config_", uid , ".yaml")
+create_config(path = config_file,
+              description = coderun_description,
+              input_namespace = namespace1,
+              output_namespace = namespace1)
+add_write(path = config_file,
+          data_product = data_product1,
+          description = dataproduct_description,
+          file_type = "csv")
+add_write(path = config_file,
+          data_product = data_product2,
+          description = dataproduct_description,
+          file_type = "csv")
+
+# CLI functions
+fair_pull(path = config_file)
+fair_run(path = config_file, skip = TRUE)
+
+# Initialise code run
+config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
+script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+handle <- initialise(config, script)
+
+# Write data
+path1 <- link_write(handle, data_product1)
+uid1 <- paste0(uid, "_1")
+df1 <- data.frame(a = uid1, b = uid1)
+write.csv(df1, path1)
+
+path2 <- link_write(handle, data_product2)
+uid2 <- paste0(uid, "_2")
+df2 <- data.frame(a = uid2, b = uid2)
+write.csv(df2, path2)
+
+finalise(handle)
+
+# Output globbing
+
+data_product3 <- file.path("real", "data", uid, "*")
+use_version <- "${{MAJOR}}"
+
+# User written config file
+config_file <- paste0("config_files/outputglobbing/config2_", uid , ".yaml")
+create_config(path = config_file,
+              description = coderun_description,
+              input_namespace = namespace1,
+              output_namespace = namespace1)
+add_write(path = config_file,
+          data_product = data_product3,
+          description = dataproduct_description,
+          file_type = "csv",
+          use_version = use_version)
+
+# CLI functions
+fair_pull(path = config_file)
+fair_run(path = config_file, skip = TRUE)
+
+# Initialise code run
+config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
+script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+handle <- initialise(config, script)
+
+data_product4 <- file.path(dirname(data_product3), "new")
+
+# Write data
+path4 <- link_write(handle = handle,
+                    data_product = data_product4)
+uid4 <- paste0(uid, "_1")
+df4 <- data.frame(a = uid4, b = uid4)
+write.csv(df4, path4)
+
+path5 <- link_write(handle = handle,
+                    data_product = data_product2)
+uid5 <- paste0(uid, "_1")
+df5 <- data.frame(a = uid5, b = uid5)
+write.csv(df5, path5)
+
+finalise(handle)
