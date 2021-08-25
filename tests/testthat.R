30c9a5794100eb5b37e8fda0121e0aa6bb2e54e5
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-25 15:01:52 +0100

    try to put CI online

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,15 +1,13 @@
-# library(testthat)
+library(testthat)
 library(rDataPipeline)
 
-
 Sys.setenv(FDP_endpoint = "http://localhost:8000/api/")
-# Sys.setenv(FDP_endpoint = "https://data.scrc.uk/api/")
-# testthat::test_check("rDataPipeline")
-
 endpoint <- Sys.getenv("FDP_endpoint")
-# if (grepl("localhost", endpoint)) run_server()
+run_server()
 
-# rFDP::fair_init(
-#   name = "Sonia Mitchell",
-#   identifier = "https://orcid.org/0000-0003-1536-2066",
-#   endpoint = endpoint)
+rFDP::fair_init(
+  name = "Sonia Mitchell",
+  identifier = "https://orcid.org/0000-0003-1536-2066",
+  endpoint = endpoint)
+
+testthat::test_check("rDataPipeline")
