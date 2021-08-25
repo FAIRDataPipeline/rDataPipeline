6333c57648893391b9d422435d2e135b2d1a0efd
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-25 14:15:40 +0100

    update package name

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 # library(testthat)
-library(rFDP)
+library(rDataPipeline)
 
 
 Sys.setenv(FDP_endpoint = "http://localhost:8000/api/")
 # Sys.setenv(FDP_endpoint = "https://data.scrc.uk/api/")
-# testthat::test_check("rFDP")
+# testthat::test_check("rDataPipeline")
 
 endpoint <- Sys.getenv("FDP_endpoint")
 # if (grepl("localhost", endpoint)) run_server()
