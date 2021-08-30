db601d3ef17417e77868051479f10152b5723138
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-30 20:32:45 +0100

    remove files

--- R/get_loaded_package_version.R
+++
@@ -1,9 +0,0 @@
-#' Get loaded package version
-#'
-#' @param package a \code{string} specifying the package name
-#'
-#' @return Returns the current package version
-#'
-get_loaded_package_version <- function(package = "SCRCdataAPI"){
-  utils::packageVersion(package)
-}
