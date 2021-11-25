171abe080443e1ffcbd41521428312605c5fb173
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-11-25 13:42:15 +0000

    Update tests [skip ci]

--- tests/testthat/test_read_table.R
+++ tests/testthat/test_read_table.R
@@ -1,36 +1,32 @@
 context("Testing read_table()")
 
-uid <- random_hash()
-coderun_description <- "Test read_table"
+coderun_description <- "Testing read_table()"
 dataproduct_description <- "A test table"
-data_product1 <- paste("test/table", uid, sep = "_")
+uid <- random_hash()
+data_product <- paste("test/table", uid, sep = "_")
 component <- "a/b/c/d"
 component2 <- "component2"
 version1 <- "0.1.0"
-namespace1 <- "username"
-
-endpoint <- Sys.getenv("FDP_endpoint")
 
 # Write test/array v.0.1.0 'username' namespace ---------------------------
 
-# User written config file
-config_file <- file.path(tempdir(), "config_files", "read_table",
-                         paste0("config_", uid, ".yaml"))
-create_config(path = config_file,
+# Generate user-written config file
+config_file <- paste0(tempfile(), ".yaml")
+create_config(init_yaml = Sys.getenv("INIT_YAML"),
+              path = config_file,
               description = coderun_description,
-              input_namespace = namespace1,
-              output_namespace = namespace1)
-add_write(path = config_file,
-          data_product = data_product1,
+              script = "echo hello") %>%
+add_write(data_product = data_product,
           description = dataproduct_description,
           version = version1)
 
-# CLI functions
-fair_run(path = config_file, skip = TRUE)
+# Generate working config file
+cmd <- paste("fair run", config_file, "--ci")
+working_config_dir <- system(cmd, intern = TRUE)
 
 # Initialise code run
-config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
-script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+config <- file.path(working_config_dir, "config.yaml")
+script <- file.path(working_config_dir, "script.sh")
 handle <- initialise(config, script)
 
 # Write data
@@ -39,7 +35,7 @@ rownames(df) <- 1:2
 
 write_table(df = df,
             handle = handle,
-            data_product = data_product1,
+            data_product = data_product,
             component = component,
             description = "Some description")
 
@@ -48,7 +44,7 @@ rownames(df2) <- 3:4
 
 write_table(df = df2,
             handle = handle,
-            data_product = data_product1,
+            data_product = data_product,
             component = component2,
             description = "Some description")
 
@@ -57,37 +53,36 @@ finalise(handle)
 
 # Start tests -------------------------------------------------------------
 
-# User written config file
-config_file <- file.path(tempdir(), "config_files", "read_table",
-                         paste0("config2_", uid, ".yaml"))
-create_config(path = config_file,
+# Generate user-written config file
+config_file <- paste0(tempfile(), ".yaml")
+create_config(init_yaml = Sys.getenv("INIT_YAML"),
+              path = config_file,
               description = coderun_description,
-              input_namespace = namespace1,
-              output_namespace = namespace1)
-add_read(path = config_file,
-         data_product = data_product1,
+              script = "echo hello") %>%
+add_read(data_product = data_product,
          use_version = version1)
 
-# CLI functions
-fair_run(path = config_file, skip = TRUE)
+# Generate working config file
+cmd <- paste("fair run", config_file, "--ci")
+working_config_dir <- system(cmd, intern = TRUE)
 
 # Initialise code run
-config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
-script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+config <- file.path(working_config_dir, "config.yaml")
+script <- file.path(working_config_dir, "script.sh")
 handle <- initialise(config, script)
 
 # Run tests
 test_that("df is returned", {
   testthat::expect_true(is.null(handle$inputs))
   tmp1 <- read_table(handle = handle,
-                     data_product = data_product1,
+                     data_product = data_product,
                      component = component)
   testthat::expect_equivalent(as.data.frame(tmp1), df)
   testthat::expect_false(is.null(handle$inputs))
   testthat::expect_equal(nrow(handle$inputs), 1)
-  testthat::expect_equal(handle$inputs$data_product, data_product1)
+  testthat::expect_equal(handle$inputs$data_product, data_product)
   tmp2 <- read_table(handle = handle,
-                     data_product = data_product1,
+                     data_product = data_product,
                      component = component)
   testthat::expect_equal(nrow(handle$inputs), 1)
   testthat::expect_equal(tmp1, tmp2)
@@ -95,7 +90,7 @@ test_that("df is returned", {
 
 test_that("df2 is returned", {
   tmp <- read_table(handle = handle,
-                    data_product = data_product1,
+                    data_product = data_product,
                     component = component2)
   expect_equivalent(as.data.frame(tmp), df2)
 })
