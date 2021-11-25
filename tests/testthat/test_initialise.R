f6f160305bd79658d1ed7fe27485fec2119fb13e
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-11-25 14:28:42 +0000

    update tests [skip ci]

--- tests/testthat/test_initialise.R
+++ tests/testthat/test_initialise.R
@@ -1,29 +1,23 @@
 context("Testing initialise()")
 
-uid <- as.character(random_hash())
-data_product1 <- file.path("real", "data", uid, "1")
-dataproduct_description <- "a nice description"
-coderun_description <- "Do nothing"
-namespace1 <- "username"
-
-endpoint <- Sys.getenv("FDP_endpoint")
+coderun_description <- "Testing initialise()"
 
 # delete_if_empty ---------------------------------------------------------
 
-# User written config file
-config_file <- file.path(tempdir(), "config_files", "initialise",
-                         paste0("config_", uid, ".yaml"))
-create_config(path = config_file,
+# Generate user-written config file
+config_file <- paste0(tempfile(), ".yaml")
+create_config(init_yaml = Sys.getenv("INIT_YAML"),
+              path = config_file,
               description = coderun_description,
-              input_namespace = namespace1,
-              output_namespace = namespace1)
+              script = "echo hello")
 
-# CLI functions
-fair_run(path = config_file, skip = TRUE)
+# Generate working config file
+cmd <- paste("fair run", config_file, "--ci")
+working_config_dir <- system(cmd, intern = TRUE)
 
 # Initialise code run
-config <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "config.yaml")
-script <- file.path(Sys.getenv("FDP_CONFIG_DIR"), "script.sh")
+config <- file.path(working_config_dir, "config.yaml")
+script <- file.path(working_config_dir, "script.sh")
 handle <- initialise(config, script)
 
 finalise(handle, delete_if_empty = TRUE)
