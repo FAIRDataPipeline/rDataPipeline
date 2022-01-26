b2d57a5518f629a517951a250b32634dad8f5e6c
Ryan J Field <RyanJField@users.noreply.github.com>
2022-01-26 05:13:04 +0000

    windows fix

--- R/get_token.R
+++ R/get_token.R
@@ -1,5 +1,8 @@
 #' get_token
 #'
 get_token <- function() {
+  if (Sys.getenv("FDP_LOCAL_TOKEN") != "") {
+    return(Sys.getenv("FDP_LOCAL_TOKEN"))
+  }      
   readLines(file.path("~", ".fair", "registry", "token"))
 }
